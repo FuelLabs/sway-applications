9b95d8d844eeda61d0bdcdd0f6e6fcf0da75f01a
Braqzen <103777923+Braqzen@users.noreply.github.com>
2022-05-24 06:24:37 +0100

    Update build.sh
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 PROJECT=$1
 
 if [ $PROJECT = 'escrow' ]; then
-	cd $PROJECT
-    forc build
-    forc build --path tests/artifacts/asset
+    forc build --path $PROJECT
+    forc build --path $PROJECT/tests/artifacts/asset
 fi
