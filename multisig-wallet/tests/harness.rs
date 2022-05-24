630d1f5d835fe10abe159a5a23fc048896d208f3
Braqzen <sydney.boroch@fuel.sh>
2022-05-24 07:20:14 +0100

    Removed duplicate CI & updated code for release

--- multisig-wallet/tests/harness.rs
+++ multisig-wallet/tests/harness.rs
@@ -1,30 +1,38 @@
-use fuel_tx::ContractId;
-use fuels_abigen_macro::abigen;
 use fuels::prelude::*;
-use fuels::test_helpers;
+use fuels_abigen_macro::abigen;
 
 // Load abi from json
-abigen!(MyContract, "out/debug/multisig-wallet-abi.json");
+abigen!(Multisig, "out/debug/multisig-wallet-abi.json");
 
-async fn get_contract_instance() -> (MyContract, ContractId) {
-    // Deploy the compiled contract
-    let compiled = Contract::load_sway_contract("./out/debug/multisig-wallet.bin").unwrap();
+async fn setup() -> (Multisig, LocalWallet, LocalWallet, LocalWallet) {
+    // Create some addresses with the minimum amount of asset: 1 Million
+    let (pk1, mut coins1) = setup_address_and_coins(1, 1000000);
+    let (pk2, coins2) = setup_address_and_coins(1, 1000000);
+    let (pk3, coins3) = setup_address_and_coins(1, 1000000);
 
-    // Launch a local network and deploy the contract
-    let (provider, wallet) = test_helpers::setup_test_provider_and_wallet().await;
+    coins1.extend(coins2);
+    coins1.extend(coins3);
 
-    let id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
-        .await
-        .unwrap();
+    // Launch a provider with those coins
+    let (provider, _) = setup_test_provider(coins1).await;
 
-    let instance = MyContract::new(id.to_string(), provider, wallet);
+    // Get the wallets from that provider
+    let wallet1 = LocalWallet::new_from_private_key(pk1, provider.clone());
+    let wallet2 = LocalWallet::new_from_private_key(pk2, provider.clone());
+    let wallet3 = LocalWallet::new_from_private_key(pk3, provider);
 
-    (instance, id)
+    let id = Contract::deploy(
+        "./out/debug/multisig.bin",
+        &wallet1,
+        TxParameters::default(),
+    )
+    .await
+    .unwrap();
+
+    (Multisig::new(id.to_string(), wallet1.clone()), wallet1, wallet2, wallet3)
 }
 
 #[tokio::test]
 async fn can_get_contract_id() {
-    let (_instance, _id) = get_contract_instance().await;
-
-    // Now you have an instance of your contract you can use to test each function
+    let (multisig, wallet1, wallet2, wallet3) = setup().await;
 }
