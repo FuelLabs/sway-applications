4b5d800c6310b2bb652934368555c0c1ad391b7b
Braqzen <sydney.boroch@fuel.sh>
2022-06-04 22:24:02 +0100

    Added to CI

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -7,4 +7,6 @@ if [ $PROJECT = 'escrow' ]; then
     forc build --path $PROJECT/tests/artifacts/asset
 elif [ $PROJECT = 'multisig-wallet' ]; then
     forc build --path $PROJECT
+elif [ $PROJECT = 'crowdfund' ]; then
+    forc build --path $PROJECT
 fi
