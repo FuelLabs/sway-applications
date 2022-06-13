ad6383ba0a727f1ae5695f666212940078dc1a50
bitzoic <cameron.carstens@fuel.sh>
2022-06-13 16:05:42 -0400

    Add reserve function tests

--- english-auction/tests/harness.rs
+++ english-auction/tests/harness.rs
@@ -1825,3 +1825,40 @@ mod deposits {
 //         );
 //     }
 // }
+
+mod reserve {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn gets_reserve() {
+        let (deploy_wallet, seller, _buyer1, _buyer2, sell_asset_id, buy_asset_id, sell_amount, inital_price, reserve_price, time) = setup().await;
+
+        init(&deploy_wallet,
+            &seller,
+            sell_asset_id,
+            sell_amount,
+            buy_asset_id,
+            inital_price,
+            reserve_price,
+            time
+        )
+        .await;
+
+        assert_eq!(
+            deploy_wallet.auction.reserve().call().await.unwrap().value,
+            reserve_price
+        );
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn panics_when_not_initalized() {
+        let (deploy_wallet, _seller, _buyer1, _buyer2, _sell_asset_id, _buy_asset_id, _sell_amount, _inital_price, reserve_price, _time) = setup().await;
+
+        assert_eq!(
+            deploy_wallet.auction.reserve().call().await.unwrap().value,
+            reserve_price
+        );
+    }
+}
