35a044fbfdda91d83dc5964b13c7776e45d2548f
bitzoic <cameron.carstens@fuel.sh>
2022-06-13 15:51:59 -0400

    Add current bid tests

--- english-auction/tests/harness.rs
+++ english-auction/tests/harness.rs
@@ -1620,3 +1620,60 @@ mod auction_end_block {
         );
     }
 }
+
+mod current_bid {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn gets_current_bid() {
+        let (deploy_wallet, seller, buyer1, _buyer2, sell_asset_id, buy_asset_id, sell_amount, inital_price, reserve_price, time) = setup().await;
+
+        init(&deploy_wallet,
+            &seller,
+            sell_asset_id,
+            sell_amount,
+            buy_asset_id,
+            inital_price,
+            reserve_price,
+            time
+        )
+        .await;
+
+        assert_eq!(
+            deploy_wallet.auction.current_bid().call().await.unwrap().value,
+            0
+        );
+
+        deploy_funds(&buyer1, &buyer1.wallet, 100).await;
+
+        let tx_params = TxParameters::new(None, Some(1_000_000), None, None);
+        let call_params = CallParameters::new(Some(inital_price), Some(AssetId::from(*buy_asset_id)));
+
+        let _bid1 = buyer1
+            .auction
+            .bid()
+            .tx_params(tx_params)
+            .call_params(call_params)
+            .call()
+            .await
+            .unwrap()
+            .value;
+
+        assert_eq!(
+            buyer1.auction.current_bid().call().await.unwrap().value,
+            inital_price
+        ); 
+    }
+
+    #[tokio::test]
+    #[should_panic]
+    async fn panics_when_not_initalized() {
+        let (deploy_wallet, _seller, _buyer1, _buyer2, _sell_asset_id, _buy_asset_id, _sell_amount, _inital_price, _reserve_price, _time) = setup().await;
+
+        assert_eq!(
+            deploy_wallet.auction.current_bid().call().await.unwrap().value,
+            0
+        );
+    }
+}
