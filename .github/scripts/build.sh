23f3ad9d3b92ee4bae5fd7db3a7e3d477618910e
Braqzen <sydney.boroch@fuel.sh>
2022-06-19 21:30:29 +0100

    Updating build to include asset

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -7,6 +7,7 @@ if [ $PROJECT = 'escrow' ]; then
     forc build --path $PROJECT/tests/artifacts/asset
 elif [ $PROJECT = 'fundraiser' ]; then
     forc build --path $PROJECT
+    forc build --path $PROJECT/tests/artifacts/asset
 elif [ $PROJECT = 'multisig-wallet' ]; then
     forc build --path $PROJECT
 fi
