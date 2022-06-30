b07672d6772e23a54a1c250d69ac6dcd1601b8ae
Matt Auer <mattauer@umich.edu>
2022-06-30 14:41:42 -0400

    refactor: ignore test

--- dao-voting/tests/harness.rs
+++ dao-voting/tests/harness.rs
@@ -515,37 +515,38 @@ mod execute {
     use super::*;
 
     mod success {
-        //use super::*;
+        use super::*;
 
         // TODO: uncomment this test once more support is added for the CALL opcode
-        // #[tokio::test]
-        // async fn user_proposal_can_execute() {
-        //     let (_gov_token, gov_token_id, deployer, user, asset_amount) = setup().await;
-        //     constructor(&deployer, gov_token_id).await;
+        #[tokio::test]
+        #[ignore]
+        async fn user_proposal_can_execute() {
+            let (_gov_token, gov_token_id, deployer, user, asset_amount) = setup().await;
+            constructor(&deployer, gov_token_id).await;
 
-        //     mint(
-        //         &deployer.gov_token.as_ref().unwrap(),
-        //         100,
-        //         user.wallet.address(),
-        //     )
-        //     .await;
+            mint(
+                &deployer.gov_token.as_ref().unwrap(),
+                100,
+                user.wallet.address(),
+            )
+            .await;
 
-        //     let tx_params = TxParameters::new(None, Some(1_000_000), None, None);
-        //     let call_params = CallParameters::new(
-        //         Some(asset_amount),
-        //         Some(AssetId::from(*gov_token_id)),
-        //         Some(100_000),
-        //     );
-        //     deposit(&user, tx_params, call_params).await;
+            let tx_params = TxParameters::new(None, Some(1_000_000), None, None);
+            let call_params = CallParameters::new(
+                Some(asset_amount),
+                Some(AssetId::from(*gov_token_id)),
+                Some(100_000),
+            );
+            deposit(&user, tx_params, call_params).await;
 
-        //     let proposal_transaction = proposal(gov_token_id);
-        //     create_proposal(&user, 10, 1, proposal_transaction.clone()).await;
-        //     vote(&user, true, 0, asset_amount / 2).await;
+            let proposal_transaction = proposal_transaction(gov_token_id);
+            create_proposal(&user, 10, 1, proposal_transaction.clone()).await;
+            vote(&user, true, 0, asset_amount / 2).await;
 
-        //     execute(&user, 0).await;
+            execute(&user, 0).await;
 
-        //     // TODO actually test execution of an arbitrary transaction
-        // }
+            // TODO actually test execution of an arbitrary transaction
+        }
     }
 
     mod revert {
