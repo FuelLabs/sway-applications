c5d8dc0a8b8256a60c58099cebfbfdb253592422
simonr0204 <simon.roberts@fuel.sh>
2022-07-06 16:51:11 -0400

    otc swap predicate

---
+++ otc_swap_predicate/src/main.sw
@@ -0,0 +1,65 @@
+predicate;
+
+use std::{
+    address::Address,
+    assert::assert,
+    constants::BASE_ASSET_ID,
+    contract_id::ContractId,
+    result::Result,
+    tx::{b256_from_pointer_offset, tx_input_coin_owner, tx_output_amount, tx_output_pointer}
+};
+
+/// Order / OTC swap Predicate
+///
+/// Sending tokens to this predicate root creates an "order" that anyone can fill
+/// This coin can be unlocked by any transaction which has an output which meets the conditions of the oder
+/// The order maker can "cancel" the order by spending the predicate's coins in any transaction containing an input they have signed
+///
+/// Limitations:
+///    - An order can not be partially filled - the taker must pay the entire ask amount
+///    - There is no on-chain matching engine, so an order placed 'offside' would not be matched with an existing order with a better price (on the contrary, it would be vulnerable to arbitrage)
+///
+/// As such, this mechanism is most useful for OTC trades and atomic swaps. 
+///
+/// # Arguments
+///
+/// - `input_index` - The index of the Coin input signed by the order maker, if the order is to be cancelled.
+/// - `output_index` - The index of the Coin output which pays the order maker.
+///
+fn main(input_index: u8, output_index: u8) -> bool {
+    
+    // Order conditions: This must be hardcoded here.
+    // The spending transaction must have an output that sends `ask_amount` of `ask_token` to `maker`
+    let maker = ~Address::from(0x0303030303030303030303030303030303030303030303030303030303030303);
+    let ask_amount = 42;
+    let ask_token = BASE_ASSET_ID;
+
+    // First, check if the transaction contains an input coin from the maker, to cancel their own order:
+    let taker = tx_input_coin_owner(input_index);
+
+    if (taker == maker) {
+        true
+    }
+
+    else {
+        // Otherwise, evaluate the terms of the order:
+
+        let amount = tx_output_amount(output_index);
+
+        // Get the token contract ID and receiver from the output at the given index
+        let output_pointer = tx_output_pointer(output_index);
+
+        // `Output::Coin` is serialized as :
+        //    `type`     (8 bytes)
+        //    `to`       (32 bytes)
+        //    `amount`   (8 bytes)
+        //    `asset_id` (32 bytes)
+        // Offsets from the output pointer to each property are set accordingly:
+
+        let to = ~Address::from(b256_from_pointer_offset(output_pointer, 8));
+        let asset_id = ~ContractId::from(b256_from_pointer_offset(output_pointer, 48));
+
+        // Evaluate the predicate
+        (to == maker) && (amount == ask_amount) && (asset_id == ask_token)
+    }
+}
---
+++ otc_swap_predicate/tests/harness.rs
@@ -0,0 +1,143 @@
+use fuel_core::service::Config;
+use fuel_gql_client::fuel_tx::{AssetId, Contract, Input, Output, Transaction};
+use fuel_gql_client::fuel_vm::{consts::REG_ONE, prelude::Opcode};
+use fuels::prelude::*;
+use fuels_contract::script::Script;
+
+async fn get_balance(provider: &Provider, address: Address, asset: AssetId) -> u64 {
+    let balance = provider.get_asset_balance(&address, asset).await.unwrap();
+    balance
+}
+
+#[tokio::test]
+async fn otc_swap_with_predicate() {
+    // Set up a wallet and send some base asset to the predicate root
+    let base_asset: AssetId = Default::default();
+
+    let mut provider_config = Config::local_node();
+    provider_config.predicates = true; // predicates are currently disabled by default
+    let wallet = launch_custom_provider_and_get_single_wallet(Some(provider_config)).await;
+
+    // Get provider and client
+    let provider = wallet.get_provider().unwrap();
+    let client = &provider.client;
+
+    // Get predicate bytecode and root
+    let predicate_bytecode =
+        std::fs::read("../otc_swap_predicate/out/debug/OTC_swap_predicate.bin").unwrap();
+    let predicate_root: [u8; 32] = (*Contract::root_from_code(&predicate_bytecode)).into();
+    let predicate_root = Address::from(predicate_root);
+
+    // Transfer some coins to the predicate root
+    let offered_amount = 1000;
+
+    let _receipt = wallet
+        .transfer(
+            &predicate_root,
+            offered_amount,
+            base_asset,
+            TxParameters::default(),
+        )
+        .await
+        .unwrap();
+
+    let initial_predicate_balance = get_balance(&provider, predicate_root, base_asset).await;
+    let initial_wallet_balance = get_balance(&provider, wallet.address(), base_asset).await;
+    let receiver_address = Address::new([3u8; 32]);
+    let initial_receiver_balance = get_balance(&provider, receiver_address, base_asset).await;
+
+    // The predicate root has received the coin
+    assert_eq!(initial_predicate_balance, offered_amount);
+
+    // Get predicate coin to unlock
+    let predicate_coin = &provider.get_coins(&predicate_root).await.unwrap()[0];
+    let predicate_coin_utxo_id = predicate_coin.utxo_id.clone().into();
+
+    // Get other coin to spend
+    let swap_coin = &provider.get_coins(&wallet.address()).await.unwrap()[0];
+    let swap_coin_utxo_id = swap_coin.utxo_id.clone().into();
+    let swap_coin_amount: u64 = swap_coin.amount.clone().into();
+
+    // Configure inputs and outputs to send coins from the predicate root to another address
+
+    // The predicate allows to spend its tokens if `ask_amount` is sent to the offer maker.
+    // This must match the amount in the predicate
+    let ask_amount = 42;
+
+    // This is the coin belonging to the predicate root
+    let input_predicate = Input::CoinPredicate {
+        utxo_id: predicate_coin_utxo_id,
+        owner: predicate_root,
+        amount: offered_amount,
+        asset_id: base_asset,
+        maturity: 0,
+        predicate: predicate_bytecode,
+        predicate_data: vec![1u8, 0u8], // Predicate data is the index of the input and output that pay the receiver
+    };
+
+    // This is the coin belonging to the wallet taking the order
+    let input_from_taker = Input::CoinSigned {
+        utxo_id: swap_coin_utxo_id,
+        owner: wallet.address(),
+        amount: swap_coin_amount,
+        asset_id: base_asset,
+        witness_index: 0,
+        maturity: 0,
+    };
+
+    // A coin output for the transfer to the receiver
+    let output_to_receiver = Output::Coin {
+        to: receiver_address,
+        amount: ask_amount,
+        asset_id: base_asset,
+    };
+
+    // A coin output for the transfer to the order taker
+    let output_to_taker = Output::Coin {
+        to: wallet.address(),
+        amount: offered_amount,
+        asset_id: base_asset,
+    };
+
+    // A Change output
+    let output_change = Output::Change {
+        to: wallet.address(),
+        amount: 0,
+        asset_id: Default::default(),
+    };
+
+    let mut tx = Transaction::Script {
+        gas_price: 0,
+        gas_limit: 10_000_000,
+        maturity: 0,
+        byte_price: 0,
+        receipts_root: Default::default(),
+        script: Opcode::RET(REG_ONE).to_bytes().to_vec(),
+        script_data: vec![],
+        inputs: vec![input_predicate, input_from_taker],
+        outputs: vec![output_to_receiver, output_to_taker, output_change],
+        witnesses: vec![],
+        metadata: None,
+    };
+
+    // Sign and execute the transaction
+    wallet.sign_transaction(&mut tx).await.unwrap();
+    let script = Script::new(tx);
+    let _receipts = script.call(&client).await.unwrap();
+
+    let predicate_balance = get_balance(&provider, predicate_root, base_asset).await;
+    let wallet_balance = get_balance(&provider, wallet.address(), base_asset).await;
+    let receiver_balance = get_balance(&provider, receiver_address, base_asset).await;
+
+    // The predicate root's coin has been spent
+    assert_eq!(predicate_balance, 0);
+
+    // Receiver has been paid the ask amount
+    assert_eq!(receiver_balance, initial_receiver_balance + ask_amount);
+
+    // Taker has send `ask_amount` tokens and got `offered_amount` in return
+    assert_eq!(
+        wallet_balance,
+        initial_wallet_balance - ask_amount + offered_amount
+    );
+}
