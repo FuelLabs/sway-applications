e103a74d7596aab3e47602b8b2125a222923922f
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-08-01 18:43:17 -0500

    feat: add config file

---
+++ escrow/escrow.config.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import { createConfig, replaceEventOnEnv } from 'escrow-scripts';
+
+const { NODE_ENV, OUTPUT_ENV } = process.env;
+
+function getEnvName() {
+  return NODE_ENV === 'test' ? '.env.test' : '.env';
+}
+
+dotenv.config({
+  path: `./docker/${getEnvName()}`,
+});
+
+export default createConfig({
+  types: {
+    artifacts: './packages/contracts/**/out/debug/**-abi.json',
+    output: './packages/frontend/src/types/contracts',
+  },
+  contracts: [
+    {
+      name: 'VITE_CONTRACT_ID',
+      path: './packages/contracts',
+    },
+    {
+      name: 'VITE_TOKEN_ID',
+      path: './packages/contracts/tests/artifacts/asset',
+    },
+  ],
+  onSuccess: (event) => {
+    replaceEventOnEnv(`./packages/frontend/${OUTPUT_ENV || getEnvName()}`, event);
+  },
+});
