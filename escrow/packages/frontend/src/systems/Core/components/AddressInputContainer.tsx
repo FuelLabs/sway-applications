5359a956fac6b763bd8bdda4cf232225bf02a044
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-08-03 14:45:26 -0500

    refactor: update for new escrow contracts

--- escrow/packages/frontend/src/systems/Core/components/AddressInputContainer.tsx
+++ escrow/packages/frontend/src/systems/Core/components/AddressInputContainer.tsx
@@ -2,32 +2,22 @@ import { Button, Input } from "@fuel-ui/react";
 import type { ChangeEvent } from "react";
 
 interface Props {
-    onUserInfoChange: (event: ChangeEvent<HTMLInputElement>, userIdx: number) => void;
-    onAddUser: (event: any) => void;
-    onRemoveUser: (userId: number) => void;
-    users: string[];
+    onArbiterAddressChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    arbiter: string;
 }
 
 export const AddressInputContainer = (props: Props) => {
     return (
-        <>
-            {props.users.map((user, i) => (
-                <Input css={{ alignSelf: "stretch" }} >
-                    <Input.Field
-                        id={`user${i}`}
-                        name={`user${i}`}
-                        placeholder={`User ${i} Address`}
-                        value={user}
-                        type="text"
-                        onChange={(e) => props.onUserInfoChange(e, i)}
-                        css={{ font: "$sans" }}
-                    />
-                    <Input.ElementRight>
-                        <Button color="tomato" leftIcon="DividerHorizontalIcon" onPress={() => props.onRemoveUser(i)} />
-                    </Input.ElementRight>
-                </Input>
-            ))}
-            <Button leftIcon="PlusIcon" css={{ font: "$sans", width: "50%" }} onPress={props.onAddUser}>Add User</Button>
-        </>
+        <Input css={{ alignSelf: "stretch" }} >
+            <Input.Field
+                id={`arbiter`}
+                name={`arbiter`}
+                placeholder={`Arbiter Address`}
+                value={props.arbiter}
+                type="text"
+                onChange={(e) => props.onArbiterAddressChange(e)}
+                css={{ font: "$sans" }}
+            />
+        </Input>
     );
 }
