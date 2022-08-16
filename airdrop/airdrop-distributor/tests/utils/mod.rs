096a134224c4650a59d2981f26080f6b7df31e5a
bitzoic <cameron.carstens@fuel.sh>
2022-08-16 16:02:23 -0400

    Add getter tests and Asset struct

--- airdrop/airdrop-distributor/tests/utils/mod.rs
+++ airdrop/airdrop-distributor/tests/utils/mod.rs
@@ -4,18 +4,22 @@ use fuels::{contract::contract::CallResponse, prelude::*};
 abigen!(AirdropDistributor, "out/debug/airdrop-distributor-abi.json");
 abigen!(SimpleToken, "../simple-token/out/debug/simple-token-abi.json");
 
+pub struct Asset {
+    pub token: SimpleToken,
+    pub asset_id: ContractId,
+}
+
 pub struct Metadata {
-    airdrop_distributor: AirdropDistributor,
-    asset: Option<SimpleToken>,
-    wallet: LocalWallet,
+    pub airdrop_distributor: AirdropDistributor,
+    pub wallet: LocalWallet,
 }
 
 pub mod test_helpers{
 
     use super::*;
 
-    pub async fn setup() -> (Metadata, Metadata, Metadata, Metadata, Metadata, Metadata) {
-        let num_wallets = 6;
+    pub async fn setup() -> (Metadata, Metadata, Metadata, Metadata, Asset) {
+        let num_wallets = 4;
         let coins_per_wallet = 1;
         let coin_amount = 1000000;
         let mut wallets = launch_custom_provider_and_get_wallets(
@@ -32,8 +36,6 @@ pub mod test_helpers{
         let wallet2 = wallets.pop().unwrap();
         let wallet3 = wallets.pop().unwrap();
         let wallet4 = wallets.pop().unwrap();
-        let wallet5 = wallets.pop().unwrap();
-        let wallet6 = wallets.pop().unwrap();
     
         let airdrop_distributor_id = Contract::deploy(
             "./out/debug/airdrop-distributor.bin",
@@ -57,41 +59,30 @@ pub mod test_helpers{
 
         let deployer = Metadata {
             airdrop_distributor: AirdropDistributorBuilder::new(airdrop_distributor_id.to_string(), wallet1.clone()).build(),
-            asset: Some(SimpleTokenBuilder::new(simple_token_id.to_string(), wallet1.clone()).build()),
             wallet: wallet1.clone()
         };
 
         let user1 = Metadata {
             airdrop_distributor: AirdropDistributorBuilder::new(airdrop_distributor_id.to_string(), wallet2.clone()).build(),
-            asset: Some(SimpleTokenBuilder::new(simple_token_id.to_string(), wallet2.clone()).build()),
             wallet: wallet2.clone()
         };
 
         let user2 = Metadata {
             airdrop_distributor: AirdropDistributorBuilder::new(airdrop_distributor_id.to_string(), wallet3.clone()).build(),
-            asset: Some(SimpleTokenBuilder::new(simple_token_id.to_string(), wallet3.clone()).build()),
             wallet: wallet3.clone()
         };
 
         let user3 = Metadata {
             airdrop_distributor: AirdropDistributorBuilder::new(airdrop_distributor_id.to_string(), wallet4.clone()).build(),
-            asset: Some(SimpleTokenBuilder::new(simple_token_id.to_string(), wallet4.clone()).build()),
             wallet: wallet4.clone()
         };
 
-        let user4 = Metadata {
-            airdrop_distributor: AirdropDistributorBuilder::new(airdrop_distributor_id.to_string(), wallet5.clone()).build(),
-            asset: Some(SimpleTokenBuilder::new(simple_token_id.to_string(), wallet5.clone()).build()),
-            wallet: wallet5.clone()
+        let asset = Asset {
+            token: SimpleTokenBuilder::new(simple_token_id.to_string(), wallet1.clone()).build(),
+            asset_id: ContractId::new(*simple_token_id.hash()),
         };
 
-        let user5 = Metadata {
-            airdrop_distributor: AirdropDistributorBuilder::new(airdrop_distributor_id.to_string(), wallet6.clone()).build(),
-            asset: Some(SimpleTokenBuilder::new(simple_token_id.to_string(), wallet6.clone()).build()),
-            wallet: wallet6.clone()
-        };
-
-        (deployer, user1, user2, user3, user4, user5)
+        (deployer, user1, user2, user3, asset)
     }
 }
 
@@ -115,6 +106,14 @@ pub mod airdrop_distributor_abi_calls {
         token: ContractId
     ) -> CallResponse<()> {
         contract.constructor(claim_time, merkle_root, token).call().await.unwrap()
+    }
+
+    pub async fn end_block(contract: &AirdropDistributor) -> u64 {
+        contract.end_block().call().await.unwrap().value
+    }
+
+    pub async fn merkle_root(contract: &AirdropDistributor) -> [u8; 32] {
+        contract.merkle_root().call().await.unwrap().value
     }
 }
 
