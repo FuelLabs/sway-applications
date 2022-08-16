0aa46b7b1702f4904b8540082e7d562d1c7d85f7
bitzoic <cameron.carstens@fuel.sh>
2022-08-16 16:06:36 -0400

    Add test functions folder and files

---
+++ airdrop/airdrop-distributor/tests/functions/claim.rs
@@ -0,0 +1,16 @@
+use crate::utils::{
+    airdrop_distributor_abi_calls::{constructor, claim},
+    test_helpers::setup,
+}
+
+mod success {
+
+    use super::*;
+
+}
+
+mod revert {
+
+    use super::*;
+
+}
---
+++ airdrop/airdrop-distributor/tests/functions/constructor.rs
@@ -0,0 +1,16 @@
+use crate::utils::{
+    airdrop_distributor_abi_calls::{constructor, end_block, merkle_root},
+    test_helpers::setup,
+};
+
+mod success {
+
+    use super::*;
+
+}
+
+mod revert {
+
+    use super::*;
+
+}
