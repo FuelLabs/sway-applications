b41346cc5e71c701a9420e3fd321fcd4561e7d86
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-08-23 16:23:02 -0500

    fix: ci

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -15,6 +15,6 @@ elif [ $PROJECT = 'multisig-wallet' ]; then
     forc build --path $PROJECT
 elif [ $PROJECT = 'NFT' ]; then
     forc build --path $PROJECT
-elid [ $PROJECT = 'oracle']; then
+elif [ $PROJECT = 'oracle']; then
     forc build --path $PROJECT
 fi
