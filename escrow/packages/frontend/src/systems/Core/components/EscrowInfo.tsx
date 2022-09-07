a50f049406dd2a1ffdbfc88917ec6c34ee2d8810
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-09-07 17:28:20 -0500

    refactor: how info is displayed

--- escrow/packages/frontend/src/systems/Core/components/EscrowInfo.tsx
+++ escrow/packages/frontend/src/systems/Core/components/EscrowInfo.tsx
@@ -5,66 +5,32 @@ import { DECIMAL_PLACES } from "@/config";
 import type { EscrowInfoOutput } from "@/types/contracts/EscrowAbi"
 
 interface EscrowInfoProps {
-    escrows: EscrowInfoOutput[] | undefined
+    escrows: EscrowInfoOutput[];
 }
 
-export function EscrowInfo({ escrows }: EscrowInfoProps) {
+export function EscrowInfo({
+    escrows,
+}: EscrowInfoProps) {
 
     return (
-        <Card css={{ flex: "1", maxW: "900px", marginTop: "$5" }}>
-            {(!!escrows && escrows.length > 0)
-                ? <>
-                    <Card.Header>
-                        Buyer Escrows
-                    </Card.Header>
-                    <Card.Body>
-                        <div>{`Arbiter: ${!!escrows[0].arbiter.address.Address ? escrows[0].arbiter.address.Address?.value : escrows[0].arbiter.address.ContractId!.value}`}</div>
-                        <div>{`Arbiter Asset: ${escrows[0].arbiter.asset.value}`}</div>
-                        <div>{`Arbiter Fee: ${formatValue(escrows[0].arbiter.fee_amount, DECIMAL_PLACES)}`}</div>
-                        {escrows[0].assets.map((assetInfo) => {
-                            return <>
-                                <div>{`Asset Id: ${assetInfo.id.value}`}</div>
-                                <div>{`Asset Amount: ${formatValue(assetInfo.amount, DECIMAL_PLACES)}`}</div>
-                            </>
-                        })}
-                        <div>{`Buyer: ${!!escrows[0].buyer.address.Address ? escrows[0].buyer.address.Address?.value : escrows[0].buyer.address.ContractId?.value}`}</div>
-                        {/** TODO fix buyer.asset.None/Some is undefined */}
-                        <div>{`Buyer Desposit Asset: ${!escrows[0].buyer.asset ? "None" : escrows[0].buyer.asset.values}`}</div>
-                        <div>{`Buyer Deposit Amount: ${formatValue(escrows[0].buyer.deposited_amount, DECIMAL_PLACES)}`}</div>
-                        <div>{`Seller: ${!!escrows[0].seller.address.Address ? escrows[0].seller.address.Address?.value : escrows[0].seller.address.ContractId?.value}`}</div>
-                        <div>{`Deadline: ${escrows[0].deadline.toString()}`}</div>
-                        <div>{`Disputed: ${escrows[0].disputed}`}</div>
-                        <div>{`State: ${!!escrows[0].state.Pending ? "Pending" : "Completed"}`}</div>
-                    </Card.Body>
-
-                    <Card.Footer justify="space-evenly">
-                        {/* <Button onPress={() => handleAcceptArbiter(BigInt(0))}>
-                        Accept Arbiter
-                    </Button>
-                    <Button onPress={() => transferToSellerMutation.mutate()}>
-                        Transfer To Seller
-                    </Button>
-                    <Button onPress={() => disputeMutation.mutate()}>
-                        Dispute
-                    </Button> */}
-                    </Card.Footer>
-
-                    <Card.Footer direction="row-reverse" gap="$4">
-                        <Button leftIcon="DotsThree">
-                            Show all escrows
-                        </Button>
-                    </Card.Footer>
+        <Card.Body>
+            <div>{`Arbiter: ${!!escrows[0].arbiter.address.Address ? escrows[0].arbiter.address.Address?.value : escrows[0].arbiter.address.ContractId!.value}`}</div>
+            <div>{`Arbiter Asset: ${escrows[0].arbiter.asset.value}`}</div>
+            <div>{`Arbiter Fee: ${formatValue(escrows[0].arbiter.fee_amount, DECIMAL_PLACES)}`}</div>
+            {escrows[0].assets.map((assetInfo) => {
+                return <>
+                    <div>{`Asset Id: ${assetInfo.id.value}`}</div>
+                    <div>{`Asset Amount: ${formatValue(assetInfo.amount, DECIMAL_PLACES)}`}</div>
                 </>
-                : <>
-                    <Card.Header>
-                        Buyer Escrows
-                    </Card.Header>
-                    <Card.Body>
-                        Buyer has no Escrows
-                    </Card.Body>
-                </>
-            }
-
-        </Card>
+            })}
+            <div>{`Buyer: ${!!escrows[0].buyer.address.Address ? escrows[0].buyer.address.Address?.value : escrows[0].buyer.address.ContractId?.value}`}</div>
+            {/** TODO fix buyer.asset.None/Some is undefined */}
+            <div>{`Buyer Desposit Asset: ${!escrows[0].buyer.asset ? "None" : escrows[0].buyer.asset.values}`}</div>
+            <div>{`Buyer Deposit Amount: ${formatValue(escrows[0].buyer.deposited_amount, DECIMAL_PLACES)}`}</div>
+            <div>{`Seller: ${!!escrows[0].seller.address.Address ? escrows[0].seller.address.Address?.value : escrows[0].seller.address.ContractId?.value}`}</div>
+            <div>{`Deadline: ${escrows[0].deadline.toString()}`}</div>
+            <div>{`Disputed: ${escrows[0].disputed}`}</div>
+            <div>{`State: ${!!escrows[0].state.Pending ? "Pending" : "Completed"}`}</div>
+        </Card.Body>
     )
 }
--- escrow/packages/frontend/src/systems/Core/pages/ArbiterPage.tsx
+++ escrow/packages/frontend/src/systems/Core/pages/ArbiterPage.tsx
@@ -1,24 +1,97 @@
 import { useAtomValue } from "jotai";
 import { Flex } from "@fuel-ui/react";
-import { CreateEscrow } from "../components/CreateEscrow";
+import { css } from "@fuel-ui/css";
 
 import { Layout } from "../components/Layout";
 import { ShowBalances } from "../components/ShowBalances";
 import { showBalancesAtom } from "../jotai";
-import { Card } from "@fuel-ui/react";
+import { Card, Button, Input } from "@fuel-ui/react";
 import { useArbiterEscrows } from "../hooks/useArbiterEscrows";
 import { EscrowInfo } from "../components/EscrowInfo";
+import { useResolveDispute } from "../hooks/useResolveDispute";
+import { useState, ChangeEvent } from "react";
+import { Dropdown } from "../components/Dropdown";
 
 export default function BuyerPage() {
   const showBalances = useAtomValue(showBalancesAtom);
   const arbiterEscrows = useArbiterEscrows();
+  const [arbiterPayment, setArbiterPayment] = useState("");
+  const [favoredUser, setFavoredUser] = useState("");
+  const resolveDisputeMutation = useResolveDispute({
+    escrowId: BigInt(0),
+    arbiterPayment,
+    favoredUser
+  });
+
+  const handleAssetAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setArbiterPayment(event.target.value);
+  }
+
+  const handleUserChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFavoredUser(event.target.value);
+  }
 
   return (
     <Layout>
       <Flex css={{ flexDirection: "row", justifyContent: "center" }}>
-        <EscrowInfo escrows={arbiterEscrows} />
+        <Card css={{ flex: "1", maxW: "900px", marginTop: "$5" }}>
+          <Card.Header>
+            Arbiter Escrows
+          </Card.Header>
+          {(!!arbiterEscrows && arbiterEscrows.length > 0)
+            ? <>
+              <EscrowInfo
+                escrows={arbiterEscrows}
+              />
+
+              {(arbiterEscrows[0].disputed && arbiterEscrows[0].state.Pending) &&
+                <Card.Footer justify="space-evenly">
+                  <Input>
+                    <Input.Number
+                      placeholder="Fee Amount"
+                      value={arbiterPayment}
+                      inputMode="decimal"
+                      onChange={(e) => handleAssetAmountChange(e)}
+                    />
+                  </Input>
+                  <Dropdown className={dropDownStyle()}>
+                    Seller
+                    Buyer
+                  </Dropdown>
+                  <Button onPress={() => resolveDisputeMutation.mutate()}>
+                    Resolve Dispute
+                  </Button>
+                </Card.Footer>
+              }
+              <Card.Footer direction="row-reverse" gap="$4">
+                <Button leftIcon="DotsThree">
+                  Show all escrows
+                </Button>
+              </Card.Footer>
+            </>
+            : <>
+              <Card.Body>
+                Arbiter has no escrows
+              </Card.Body>
+            </>
+          }
+        </Card>
+
         {showBalances && <ShowBalances />}
       </Flex>
     </Layout>
   );
 }
+
+const dropDownStyle = css({
+  bg: "$accent9",
+  color: "$gray1",
+  textSize: "base",
+  font: "$sans",
+  cursor: "pointer",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "1px solid transparent",
+  borderRadius: "$lg",
+});
