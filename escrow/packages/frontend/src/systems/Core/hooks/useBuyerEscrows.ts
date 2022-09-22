16c5b8dd634af23cbd38ebb450f34538359d1afe
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-09-22 12:04:18 -0500

    refactor: use escrow hooks

--- escrow/packages/frontend/src/systems/Core/hooks/useBuyerEscrows.ts
+++ escrow/packages/frontend/src/systems/Core/hooks/useBuyerEscrows.ts
@@ -2,12 +2,12 @@ import { useQuery } from "react-query";
 
 import { useWallet } from "../context/AppContext";
 import { useContract } from "./useContract";
+import { useEscrows } from "./useEscrows";
 
 export function useBuyerEscrows() {
     const contract = useContract();
     const wallet = useWallet();
-    console.log("contract: ", contract);
-    console.log("addr: ", wallet?.address.toHexString())
+
     const { data: buyerEscrowIds } = useQuery(
         ['BuyerPage-buyerEscrowIds'],
         async () => {
@@ -18,20 +18,8 @@ export function useBuyerEscrows() {
             onError: (err) => console.log(JSON.stringify(err, null, 2)),
         }
     );
-    console.log("buyer escrow ids: ", buyerEscrowIds);
-    const { data: buyerEscrows } = useQuery(
-        ["BuyerEscrows"],
-        async () => {
-            const escrowPromises = buyerEscrowIds!.map(async escrowId => {
-                return (await contract!.functions.escrows(escrowId).call()).value
-            });
-            return await Promise.all(escrowPromises);
-        },
-        {
-            enabled: !!buyerEscrowIds,
-            onSuccess: (data) => console.log("data 2: ", data),
-            onError: (err) => console.log(JSON.stringify(err, null, 2)),
-        }
-    );
+
+    const buyerEscrows = useEscrows("BuyerEscrows", buyerEscrowIds);
+
     return buyerEscrows;
 }
--- escrow/packages/frontend/src/systems/Core/hooks/useSellerEscrows.ts
+++ escrow/packages/frontend/src/systems/Core/hooks/useSellerEscrows.ts
@@ -2,6 +2,7 @@ import { useQuery } from 'react-query';
 import { useWallet } from '../context/AppContext';
 
 import { useContract } from './useContract';
+import { useEscrows } from './useEscrows';
 
 export function useSellerEscrows() {
   const contract = useContract();
@@ -12,23 +13,8 @@ export function useSellerEscrows() {
       return contract && (await contract!.functions.seller_escrows({ Address: { value: wallet?.address!.toHexString()! } }).call()).value
     }
   );
-  //console.log("seller escrow ids: ", sellerEscrowIds);
-  //console.log("bool: ", !!sellerEscrowIds);
-  const { data: sellerEscrows } = useQuery(
-    ["SellerEscrows"],
-    async () => {
-      const escrowPromises = sellerEscrowIds!.map(async escrowId => {
-        return (await contract!.functions.escrows(escrowId).call()).value
-      }
-      );
-      return await Promise.all(escrowPromises);
-    },
-    {
-      enabled: !!sellerEscrowIds,
-      onSuccess: (data) => console.log("data: ", data),
-      onError: (err) => console.log(JSON.stringify(err, null, 4)),
-    }
-  );
-  //console.log("seller escrows: ", sellerEscrows);
+
+  const sellerEscrows = useEscrows("SellerEscrows", sellerEscrowIds);
+
   return sellerEscrows;
 }
