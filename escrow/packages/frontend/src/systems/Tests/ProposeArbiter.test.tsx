115fd551831a78a1ccd3ebaf15508a2fe88e608c
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-09-30 11:26:40 -0500

    test: user can set propose arbiter inputs

--- escrow/packages/frontend/src/systems/Tests/ProposeArbiter.test.tsx
+++ escrow/packages/frontend/src/systems/Tests/ProposeArbiter.test.tsx
@@ -118,8 +118,117 @@ describe("Propose arbiter", () => {
         expect(proposeArbiterBtn).toBeDisabled();
     });
 
-    xit("should be able to set input values", async () => {
+    it("should be able to set input values", async () => {
+        const { user } = renderWithRouter(<App />, {
+            route: "/seller",
+        });
 
+        const arbiter = wallets[1];
+        const buyer = wallets[2];
+        const newArbiter = wallets[3];
+
+        // First create an escrow
+        const showCreateEscrowBtn = await screen.findByLabelText(/Show create escrow/);
+        expect(showCreateEscrowBtn).toBeInTheDocument();
+        await user.click(showCreateEscrowBtn);
+
+        const arbiterAddressInput = await screen.findByLabelText(/Create arbiter address input/);
+        fireEvent.change(arbiterAddressInput, {
+            target: {
+                value: arbiter.address.toHexString(),
+            },
+        });
+
+        const arbiterAssetInput = await screen.findByLabelText(/Create arbiter asset input/);
+        fireEvent.change(arbiterAssetInput, {
+            target: {
+                value: ASSETS[0]
+            },
+        });
+
+        const arbiterFeeInput = await screen.findByLabelText(/Create arbiter fee input/);
+        fireEvent.change(arbiterFeeInput, {
+            target: {
+                value: "0.1",
+            },
+        });
+
+        const buyerAddressInput = await screen.findByLabelText(/Buyer address input/);
+        fireEvent.change(buyerAddressInput, {
+            target: {
+                value: buyer.address.toHexString(),
+            },
+        });
+
+        // TODO this test should get the current blockheight and add some constant
+        // Instead of hardcoding "1000".
+        const escrowDeadlineInput = await screen.findByLabelText(/Escrow deadline input/);
+        fireEvent.change(escrowDeadlineInput, {
+            target: {
+                value: "1000",
+            },
+        });
+
+        const assetInput0 = await screen.findByLabelText(/Asset input 0/);
+        fireEvent.change(assetInput0, {
+            target: {
+                value: ASSETS[0],
+            },
+        });
+
+        const assetAmount0 = await screen.findByLabelText(/Asset amount input 0/);
+        fireEvent.change(assetAmount0, {
+            target: {
+                value: "0.1",
+            },
+        });
+
+        const addAssetBtn = await screen.findByLabelText(/Add asset/);
+        expect(addAssetBtn).toBeInTheDocument();
+        await user.click(addAssetBtn);
+
+        const assetInput1 = await screen.findByLabelText(/Asset input 1/);
+        fireEvent.change(assetInput1, {
+            target: {
+                value: ASSETS[1],
+            },
+        });
+
+        const assetAmount1 = await screen.findByLabelText(/Asset amount input 1/);
+        fireEvent.change(assetAmount1, {
+            target: {
+                value: "0.2",
+            },
+        });
+
+        const submitBtn = await screen.findByLabelText(/Create escrow/);
+        expect(submitBtn).toBeInTheDocument();
+        await user.click(submitBtn);
+
+        const newArbiterAddressInput = await screen.findByLabelText(/Propose arbiter address input/);
+        fireEvent.change(newArbiterAddressInput, {
+            target: {
+                value: newArbiter.address.toHexString(),
+            },
+        });
+        expect(newArbiterAddressInput).toHaveValue(newArbiter.address.toHexString());
+
+        const newArbiterAssetInput = await screen.findByLabelText(/Propose arbiter asset input/);
+        fireEvent.change(newArbiterAssetInput, {
+            target: {
+                value: ASSETS[1],
+            },
+        });
+        expect(newArbiterAssetInput).toHaveValue(ASSETS[1]);
+
+        const newArbiterFeeInput = await screen.findByLabelText(/Propose arbiter fee input/);
+        const newArbiterFeeValue = "0.2";
+        fireEvent.change(newArbiterFeeInput, {
+            target: {
+                value: newArbiterFeeValue
+            }
+        });
+        expect(newArbiterFeeInput).toHaveValue(newArbiterFeeValue);
     });
 
     it("should be able to propose arbiter", async () => {
@@ -230,7 +339,7 @@ describe("Propose arbiter", () => {
             }
         });
 
-        const proposeArbiterBtn = await screen.findByLabelText(/Propose arbiter/);
+        const proposeArbiterBtn = await screen.findByLabelText(/Propose arbiter button/);
         expect(proposeArbiterBtn).toBeInTheDocument();
         await user.click(proposeArbiterBtn);
     });
