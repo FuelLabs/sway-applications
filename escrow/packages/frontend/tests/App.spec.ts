37320387697fe6c8a3f4a11ec4b1f08a71550dda
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-10-03 16:35:01 -0500

    test: seller can return deposit to buyer

--- escrow/packages/frontend/tests/App.spec.ts
+++ escrow/packages/frontend/tests/App.spec.ts
@@ -20,7 +20,7 @@ test.describe("e2e", () => {
         }
     });
 
-    test("Basic app flow", async ({ page }) => {
+    test("Buyer transfers to seller", async ({ page }) => {
         await page.goto('localhost:3000/seller');
 
         const arbiter = wallets[1];
@@ -76,6 +76,7 @@ test.describe("e2e", () => {
         await depositAmountInput.fill("0.1");
 
         const deposit = page.locator('[aria-label="Deposit"]');
+        expect(deposit).toContainText("Deposit Asset");
         await deposit.click();
 
         page.on('dialog', async dialog => {
@@ -90,5 +91,97 @@ test.describe("e2e", () => {
         page.on('dialog', async dialog => {
             expect(dialog.message()).toContain("Transfer to seller successful.");
         });
+    });
+
+    test("Seller returns deposit to buyer", async ({ page }) => {
+        await page.goto('localhost:3000/seller');
+
+        const arbiter = wallets[1];
+        const buyer = wallets[2];
+
+        // Seller creates escrow
+        const showCreateEscrow = page.locator('[aria-label="Show create escrow"]');
+        expect(showCreateEscrow).toContainText("Create Escrow");
+        await showCreateEscrow.click();
+
+        const arbiterAddressInput = page.locator('[aria-label="Create arbiter address input"]');
+        await arbiterAddressInput.fill(arbiter.address.toHexString());
+        const arbiterAssetInput = page.locator('[aria-label="Create arbiter asset input"]');
+        await arbiterAssetInput.fill(ASSETS[0]);
+        const arbiterFeeInput = page.locator('[aria-label="Create arbiter fee input"]');
+        await arbiterFeeInput.fill("0.1");
+
+        const buyerAddressInput = page.locator('[aria-label="Buyer address input"]')
+        await buyerAddressInput.fill(buyer.address.toHexString());
+
+        const escrowDeadlineInput = page.locator('[aria-label="Escrow deadline input"]');
+        await escrowDeadlineInput.fill("1000");
+
+        const assetInput0 = page.locator('[aria-label="Asset input 0"]');
+        assetInput0.fill(ASSETS[1]);
+        const assetAmount0 = page.locator('[aria-label="Asset amount input 0"]');
+        await assetAmount0.fill("0.1");
+
+        const addAsset = page.locator('[aria-label="Add asset"]');
+        expect(addAsset).toContainText("PlusAdd Asset");
+        await addAsset.click();
+
+        const assetInput1 = page.locator('[aria-label="Asset input 1"]');
+        await assetInput1.fill(ASSETS[2]);
+        const assetAmount1 = page.locator('[aria-label="Asset amount input 1"]')
+        await assetAmount1.fill("0.2");
+
+        const createEscrow = page.locator('[aria-label="Create escrow"]');
+        expect(createEscrow).toContainText("PlusIconCreate Escrow");
+        await createEscrow.click();
+
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toContain("New escrow created");
+        });
+
+        // TODO figure out if we need to explicitly switch wallet
+        await page.goto("localhost:3000/buyer");
+
+        // Buyer deposits
+        const depositAssetInput = page.locator('[aria-label="Asset input"]');
+        await depositAssetInput.fill(ASSETS[1]);
+        const depositAmountInput = page.locator('[aria-label="Asset amount input"]');
+        await depositAmountInput.fill("0.1");
+
+        const deposit = page.locator('[aria-label="Deposit"]');
+        expect(deposit).toContainText("Deposit Asset");
+        await deposit.click();
+
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toContain("Deposit successful.");
+        });
+
+        // Switch back to seller page
+        await page.goto("localhost:3000/seller");
+
+        // Seller returns deposit to buyer
+        const returnToBuyer = page.locator('[aria-label="Return deposit"]');
+        expect(returnToBuyer).toContainText("Return Deposit");
+        await returnToBuyer.click();
+
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toContain("Deposit returned to buyer.");
+        });
+    });
+
+    test("Arbiter resolves in favor of buyer", async ({ page }) => {
+
+    });
+
+    test("Arbiter resolves in favor of seller", async ({ page }) => {
+
+    });
+
+    test("Seller can withdraw collateral and take payment after deadline", async ({ page }) => {
+
+    });
+
+    test("Seller can propose an arbiter", async ({ page }) => {
+
     });
 });
