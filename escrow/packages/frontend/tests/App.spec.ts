cc9f1ac1cbe01b64f0575a3dc6ecf6391bb24b19
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-10-04 18:22:50 -0500

    test: propose and accept arbiter

--- escrow/packages/frontend/tests/App.spec.ts
+++ escrow/packages/frontend/tests/App.spec.ts
@@ -8,13 +8,15 @@ const coins = ASSETS.map(assetId => {
     return { assetId, amount: DECIMAL_PRECISION.mul(100) };
 });
 
+test.beforeAll(async () => {
+    for (let i = 0; i < numWallets; ++i) {
+        const wallet = new Wallet(process.env[`VITE_WALLET${i}`]!);
+        wallets.push(wallet);
+    }
+    console.log(wallets[0].address.toHexString());
+});
+
 test.describe("e2e", () => {
-    test.beforeAll(async () => {
-        for (let i = 0; i < numWallets; ++i) {
-            const wallet = new Wallet(process.env[`VITE_WALLET${i}`]!);
-            wallets.push(wallet);
-        }
-    });
 
     test.beforeEach(async ({ page }) => {
         await page.goto('localhost:3000/seller');
@@ -58,9 +60,15 @@ test.describe("e2e", () => {
         expect(createEscrow).toContainText("PlusIconCreate Escrow");
         await createEscrow.click();
 
-        page.on('dialog', async dialog => {
-            expect(dialog.message()).toContain("New escrow created");
-        });
+        // const temp = page.locator("text=Error when trying to create an escrow");
+        // expect(temp).toHaveCount(1);
+
+        // const txFeedback = page.locator('text="New escrow created."')
+        // expect(txFeedback).toHaveCount(1);
+
+        // page.on('dialog', async dialog => {
+        //     expect(dialog.message()).toContain("New escrow created");
+        // });
 
         // TODO Switch page and wallet
         await page.goto("localhost:3000/buyer");
@@ -83,17 +91,17 @@ test.describe("e2e", () => {
     test("Buyer transfers to seller", async ({ page }) => {
         // Buyer transfers to seller
         const transferToSeller = page.locator('[aria-label="Transfer to seller"]');
-        expect(transferToSeller).toBeDefined();
+        expect(transferToSeller).toContainText("Transfer To Seller");
         await transferToSeller.click();
 
-        page.on('dialog', async dialog => {
-            expect(dialog.message()).toContain("Transfer to seller successful.");
-        });
+        // const txFeedback = page.locator('text="Transfer to seller successful."')
+        // expect(txFeedback).toHaveCount(1);
     });
 
     test("Seller returns deposit to buyer", async ({ page }) => {
         // Switch back to seller page
         await page.goto("localhost:3000/seller");
+        expect(page.locator('[aria-label="Show create escrow"]')).toContainText("Create Escrow");
 
         // Seller returns deposit to buyer
         const returnToBuyer = page.locator('[aria-label="Return deposit"]');
@@ -132,7 +140,7 @@ test.describe("e2e", () => {
         });
     });
 
-    test("Arbiter resolves in favor of seller", async ({ page }) => {
+    test.fixme("Arbiter resolves in favor of seller", async ({ page }) => {
 
     });
 
@@ -141,7 +149,33 @@ test.describe("e2e", () => {
 
     });
 
-    test.fixme("Seller can propose an arbiter", async ({ page }) => {
+    test("can propose and accept arbiter", async ({ page }) => {
+        // Switch back to seller page
+        await page.goto("localhost:3000/seller");
+        expect(page.locator('[aria-label="Show create escrow"]')).toContainText("Create Escrow");
 
+        // Propose arbiter
+        const proposeArbiterAddressInput = page.locator("Propose arbiter address input");
+        proposeArbiterAddressInput.fill(wallets[3].address.toHexString());
+        const proposeArbiterAssetInput = page.locator("Propose arbiter asset input");
+        proposeArbiterAssetInput.fill(ASSETS[1]);
+        const proposeArbiterFeeInput = page.locator("Propose arbiter fee input");
+        proposeArbiterFeeInput.fill("0.2");
+        const proposeArbiter = page.locator("Propose arbiter button");
+        //expect(proposeArbiter).toContainText("Propose Arbiter");
+        await proposeArbiter.click();
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toContain("New arbiter proposed.");
+        });
+
+        await page.goto("localhost:3000/buyer");
+
+        // Accept arbiter
+        const acceptArbiter = page.locator("Accept arbiter button");
+        //expect(acceptArbiter).toContainText("Accept Arbiter");
+        await acceptArbiter.click();
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toContain("Arbiter accepted successfully.")
+        });
     });
 });
