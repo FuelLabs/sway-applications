6f7911505ce967f8ac2ebf60d30abbfa616cff44
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-10-20 10:46:11 -0500

    test: node can get price

--- oracle/packages/node/tests/functions/get_price.rs
+++ oracle/packages/node/tests/functions/get_price.rs
@@ -1,6 +1,8 @@
 mod success {
     #[tokio::test]
     async fn can_get_price() {
-        
+        let oracle_node = node::OracleNode::new(10);
+        let price = oracle_node.get_price().await;
+        assert!(price > 0);
     }
 }
--- oracle/packages/node/tests/functions/new.rs
+++ oracle/packages/node/tests/functions/new.rs
@@ -1,6 +1,6 @@
 mod success {
     #[tokio::test]
     async fn can_initialize_oracle_node() {
-        let oracle_node = node::OracleNode::new(10);
+        let _oracle_node = node::OracleNode::new(10);
     }
 }
