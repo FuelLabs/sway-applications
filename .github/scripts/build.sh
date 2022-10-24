9dfe5ada437af19400fd037368bcea39cec943f8
bitzoic <cameron.carstens@fuel.sh>
2022-10-24 19:12:55 -0400

    Update CI based on comments

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -7,13 +7,13 @@ if [ $PROJECT = 'airdrop/airdrop-distributor' ]; then
     forc build --path $PROJECT/../simple-asset/
 elif [ $PROJECT = 'airdrop/simple-asset' ]; then
     forc build --path $PROJECT
-elif [ $PROJECT = 'dao-voting' ]; then
-    forc build --path $PROJECT
-    forc build --path $PROJECT/tests/artifacts/gov_token
 elif [ $PROJECT = 'auctions/english-auction' ]; then
     forc build --path $PROJECT
     forc build --path $PROJECT/tests/artifacts/asset
     forc build --path $PROJECT/../../NFT/
+elif [ $PROJECT = 'dao-voting' ]; then
+    forc build --path $PROJECT
+    forc build --path $PROJECT/tests/artifacts/gov_token
 elif [ $PROJECT = 'escrow' ]; then
     forc build --path $PROJECT
     forc build --path $PROJECT/tests/artifacts/asset
