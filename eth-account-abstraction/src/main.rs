9ad6ca8734428323e680491701da64bd27e43baa
Kyran Rawlinson <kyranrawlinson@gmail.com>
2022-10-26 16:39:39 +0100

    Cleaned up testing signature creation methods

--- eth-account-abstraction/src/main.rs
+++ eth-account-abstraction/src/main.rs
@@ -1,8 +1,8 @@
 // mod derive_from_secp;
-mod compare_pk_strings;
+mod compare;
 
 #[tokio::main]
 async fn main() {
     // let _ = derive_from_secp::derive_from_secp();
-    compare_pk_strings::compare_pk_strings().await;
+    compare::compare().await;
 }
