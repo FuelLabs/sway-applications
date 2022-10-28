78404b57994ac17d3f402aad7de8f25e5301a0c7
supiket <bengisuozaydin@gmail.com>
2022-10-28 10:32:55 +0100

    use SDK to generate asset ids in test helpers

--- AMM/contracts/AMM/tests/utils/mod.rs
+++ AMM/contracts/AMM/tests/utils/mod.rs
@@ -1,5 +1,4 @@
 use fuels::{contract::contract::CallResponse, prelude::*};
-use std::str::FromStr;
 
 abigen!(AMM, "out/debug/AMM-abi.json");
 abigen!(Exchange, "../exchange/out/debug/exchange-abi.json");
@@ -112,16 +111,33 @@ pub mod test_helpers {
     }
 
     pub async fn setup_and_initialize() -> (WalletUnlocked, AMM, Vec<ContractId>) {
-        let (wallet, amm_instance, assets) = setup().await;
+        let (wallet, amm_instance, asset_ids) = setup().await;
 
         initialize_amm_contract(&wallet, &amm_instance).await;
 
-        (wallet, amm_instance, assets)
+        (wallet, amm_instance, asset_ids)
     }
 
     pub async fn setup() -> (WalletUnlocked, AMM, Vec<ContractId>) {
-        let wallet = launch_provider_and_get_wallet().await;
+        // setup wallet and provider
+        let mut wallet = WalletUnlocked::new_random(None);
+        let num_assets = 3;
+        let coins_per_asset = 1;
+        let amount_per_coin = 1_000_000;
+        let (coins, asset_ids) = setup_multiple_assets_coins(
+            wallet.address(),
+            num_assets,
+            coins_per_asset,
+            amount_per_coin,
+        );
+        let asset_ids_as_contract_ids = asset_ids
+            .into_iter()
+            .map(|asset_id| ContractId::new(*asset_id))
+            .collect();
+        let (provider, _socket_addr) = setup_test_provider(coins.clone(), vec![], None).await;
+        wallet.set_provider(provider);
 
+        // setup AMM contract
         let amm_contract_id = Contract::deploy(
             AMM_CONTRACT_BINARY_PATH,
             &wallet,
@@ -132,19 +148,6 @@ pub mod test_helpers {
         .unwrap();
         let amm_instance = AMM::new(amm_contract_id.to_string(), wallet.clone());
 
-        let base_asset_id = ContractId::zeroed();
-        let asset_id_1 = ContractId::from_str(
-            "0x562a05877b940cc69d7a9a71000a0cfdd79e93f783f198de893165278712a480",
-        )
-        .unwrap();
-        let asset_id_2 = ContractId::from_str(
-            "0x716c345b96f3c17234c73881c40df43d3d492b902a01a062c12e92eeae0284e9",
-        )
-        .unwrap();
-        (
-            wallet,
-            amm_instance,
-            vec![base_asset_id, asset_id_1, asset_id_2],
-        )
+        (wallet, amm_instance, asset_ids_as_contract_ids)
     }
 }
