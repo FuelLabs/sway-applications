061cae41455e48fd77caf3738f3e9512f263c061
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-11-07 14:05:56 -0600

    fix: just use singular flag

--- .github/scripts/forc-handler.sh
+++ .github/scripts/forc-handler.sh
@@ -2,38 +2,34 @@
 
 PROJECT=$1
 COMMAND=$2
-echo "$@"
-args="$@"
-echo $args
-FLAGS="${@:(3)}"
-echo $FLAGS
+FLAG=$3
 
 if [ $PROJECT = 'airdrop/airdrop-distributor' ]; then
-    forc $COMMAND --path $PROJECT $FLAGS
-    forc $COMMAND --path $PROJECT/../simple-asset/ $FLAGS
+    forc $COMMAND --path $PROJECT $FLAG
+    forc $COMMAND --path $PROJECT/../simple-asset/ $FLAG
 elif [ $PROJECT = 'airdrop/simple-asset' ]; then
-    forc $COMMAND --path $PROJECT $FLAGS
+    forc $COMMAND --path $PROJECT $FLAG
 elif [ $PROJECT = 'auctions/english-auction' ]; then
-    forc $COMMAND --path $PROJECT $FLAGS
-    forc $COMMAND --path $PROJECT/tests/artifacts/asset $FLAGS
-    forc $COMMAND --path $PROJECT/../../NFT/ $FLAGS
+    forc $COMMAND --path $PROJECT $FLAG
+    forc $COMMAND --path $PROJECT/tests/artifacts/asset $FLAG
+    forc $COMMAND --path $PROJECT/../../NFT/ $FLAG
 elif [ $PROJECT = 'dao-voting' ]; then
-    forc $COMMAND --path $PROJECT $FLAGS
-    forc $COMMAND --path $PROJECT/tests/artifacts/gov_token $FLAGS
+    forc $COMMAND --path $PROJECT $FLAG
+    forc $COMMAND --path $PROJECT/tests/artifacts/gov_token $FLAG
 elif [ $PROJECT = 'escrow' ]; then
     forc $COMMAND --path $PROJECT $FLAGS
-    forc $COMMAND --path $PROJECT/tests/artifacts/asset $FLAGS
+    forc $COMMAND --path $PROJECT/tests/artifacts/asset $FLAG
 elif [ $PROJECT = 'fundraiser' ]; then
     forc $COMMAND --path $PROJECT $FLAGS
-    forc $COMMAND --path $PROJECT/tests/artifacts/asset $FLAGS
+    forc $COMMAND --path $PROJECT/tests/artifacts/asset $FLAG
 elif [ $PROJECT = 'multisig-wallet' ]; then
     forc $COMMAND --path $PROJECT $FLAGS
 elif [ $PROJECT = 'name-registry' ]; then
-    forc $COMMAND --path $PROJECT $FLAGS
+    forc $COMMAND --path $PROJECT $FLAG
 elif [ $PROJECT = 'NFT' ]; then
-    forc $COMMAND --path $PROJECT $FLAGS
+    forc $COMMAND --path $PROJECT $FLAG
 elif [ $PROJECT = 'oracle' ]; then
-    forc $COMMAND --path $PROJECT $FLAGS
+    forc $COMMAND --path $PROJECT $FLAG
 else
     echo "project name did not match"
     exit 1
