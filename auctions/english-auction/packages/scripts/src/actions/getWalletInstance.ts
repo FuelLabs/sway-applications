aebaf7100401f6e9a6fccac87b49e045e3950d92
Matt Auer <mattauer@Matts-MacBook-Pro.local>
2022-11-09 18:53:52 -0600

    fix: scripts

--- auctions/english-auction/packages/scripts/src/actions/getWalletInstance.ts
+++ auctions/english-auction/packages/scripts/src/actions/getWalletInstance.ts
@@ -7,7 +7,7 @@ export async function getWalletInstance() {
 
   if (WALLET_SECRET) {
     log('WALLET_SECRET detected');
-    return new Wallet(WALLET_SECRET, PROVIDER_URL);
+    return Wallet.fromPrivateKey(WALLET_SECRET, PROVIDER_URL);
   }
   // If no WALLET_SECRET is informed we assume
   // We are on a test environment
