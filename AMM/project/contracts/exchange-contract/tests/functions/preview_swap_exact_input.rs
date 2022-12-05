46bd32ad3aaa3353fbb2042ba20a903dd267d0d9
supiket <bengisuozaydin@gmail.com>
2022-12-05 23:23:55 +0300

    remove _partial_ from some tests' names

--- AMM/project/contracts/exchange-contract/tests/functions/preview_swap_exact_input.rs
+++ AMM/project/contracts/exchange-contract/tests/functions/preview_swap_exact_input.rs
@@ -7,7 +7,7 @@ mod success {
     use super::*;
 
     #[tokio::test]
-    async fn previews_partial_swap_of_a() {
+    async fn previews_swap_of_a() {
         let (exchange, _wallet, amounts, _asset_c_id, _added_liquidity) =
             setup_initialize_deposit_and_add_liquidity().await;
         let input_amount = 10;
@@ -30,7 +30,7 @@ mod success {
     }
 
     #[tokio::test]
-    async fn previews_partial_swap_of_b() {
+    async fn previews_swap_of_b() {
         let (exchange, _wallet, amounts, _asset_c_id, _added_liquidity) =
             setup_initialize_deposit_and_add_liquidity().await;
         let input_amount = 10;
