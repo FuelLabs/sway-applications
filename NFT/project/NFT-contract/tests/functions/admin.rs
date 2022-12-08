11ac78faf901996747cced76466a658e51e97008
Cameron Carstens <54727135+bitzoic@users.noreply.github.com>
2022-12-08 10:10:08 -0500

    Refactor NFT Application to use the Sway-Libs NFT Library (#296)
    * Bump to forc v0.30.1
    
    * Add Sway-libs NFT, remove events, remove TODO comments
    
    * Refactor admin
    
    * Refactor approve function
    
    * Refactor approved function
    
    * Refactor balance_of function
    
    * Refactor burn function
    
    * Refactor is_approved_for_all function
    
    * Refactor max_supply function
    
    * Refactor mint function
    
    * Refactor owner_of function
    
    * Refactor meta_data function
    
    * Refactor set_admin function
    
    * Refactor set_approval_for_all function
    
    * Refactor total_minted function
    
    * Refactor transfer function
    
    * Refactor constructor and update to sway-libs v0.3.1
    
    * Remove errors
    
    * Re-add revert cases for this NFT implementation
    
    * Re-add errors
    
    * Update contarct to match previous implementation
    
    * Bump to new version of NFT lib
    
    * Rename aux abi
    
    * Update test utils for new contract structure
    
    * Update admin tests
    
    * Update approve functions
    
    * Update approved
    
    * Update balance_of tests
    
    * Update constructor tests
    
    * Update is_approved_for_all tests
    
    * Update max_supply test
    
    * Update token_metadata tests
    
    * Update mint function tests
    
    * Update owner_of function tests
    
    * Update set_admin function tests
    
    * Update set_approval_for_all tests
    
    * Update tokens_minted tests
    
    * Update transfer tests
    
    * Update burn tests
    
    * Format tests and remove warnings
    
    * Add newline
    
    * Update README to include library implementation and specification
    
    * Update Forc.toml to repo standard
    
    * Update interface inline docs
    
    * Fix CI to use correct .json file name
    
    * Set TokenMetadata to None BEFORE burning the token
    
    * Run cargo fmt
    
    * Uninitialize -> Uninitialized
    
    Co-authored-by: bitzoic <cameron.carstens@fuel.sh>

--- NFT/project/NFT-contract/tests/functions/admin.rs
+++ NFT/project/NFT-contract/tests/functions/admin.rs
@@ -12,40 +12,28 @@ mod success {
     async fn gets_admin() {
         let (deploy_wallet, owner1, _owner2) = setup().await;
 
-        // let new_admin = Option::Some(Identity::Address(owner1.wallet.address().into()));
-        let new_admin = Identity::Address(owner1.wallet.address().into());
-        constructor(true, &deploy_wallet.contract, &new_admin, 1).await;
+        assert_eq!(admin(&owner1.contract).await, None);
 
-        assert_eq!(admin(&owner1.contract).await, new_admin);
+        let new_admin = Some(Identity::Address(owner1.wallet.address().into()));
+        constructor(new_admin.clone(), &deploy_wallet.contract, Some(1)).await;
+
+        assert_eq!(admin(&owner1.contract).await, new_admin.clone());
     }
 
     #[tokio::test]
     async fn gets_admin_after_change() {
         let (deploy_wallet, owner1, owner2) = setup().await;
 
-        // let new_admin = Option::Some(Identity::Address(owner1.wallet.address().into()));
-        let new_admin = Identity::Address(owner1.wallet.address().into());
-        constructor(true, &deploy_wallet.contract, &new_admin, 1).await;
+        assert_eq!(admin(&owner1.contract).await, None);
 
-        assert_eq!(admin(&owner1.contract).await, new_admin);
+        let new_admin = Some(Identity::Address(owner1.wallet.address().into()));
+        constructor(new_admin.clone(), &deploy_wallet.contract, Some(1)).await;
 
-        // let new_admin = Option::Some(minter.clone());
-        let new_admin = Identity::Address(owner2.wallet.address().into());
-        set_admin(&owner1.contract, &new_admin).await;
+        assert_eq!(admin(&owner1.contract).await, new_admin.clone());
 
-        assert_eq!(admin(&owner1.contract).await, new_admin);
-    }
-}
+        let new_admin2 = Some(Identity::Address(owner2.wallet.address().into()));
+        set_admin(&owner1.contract, new_admin2.clone()).await;
 
-mod reverts {
-
-    use super::*;
-
-    #[tokio::test]
-    #[should_panic(expected = "Revert(18446744073709486080)")]
-    async fn when_admin_not_set() {
-        let (_deploy_wallet, owner1, _owner2) = setup().await;
-
-        admin(&owner1.contract).await;
+        assert_eq!(admin(&owner1.contract).await, new_admin2.clone());
     }
 }
