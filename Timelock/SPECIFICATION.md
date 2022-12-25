85bd38fe93734e2319ffd225180fe686c034a11c
Braqzen <sydney.boroch@fuel.sh>
2022-12-25 20:47:46 +0000

    Template commit

---
+++ Timelock/SPECIFICATION.md
@@ -0,0 +1,22 @@
+Table of Contents
+- [Overview](#overview)
+- [Use Cases](#use-cases)
+  - [Sequence diagram](#sequence-diagram)
+
+
+# Overview
+
+This document provides an overview of the application.
+
+It outlines the use cases, i.e. desirable functionality, in addition to requirements for the smart contract and the user interface.
+
+# Use Cases
+
+This section contains general information about the functionality of the application and thus does not touch upon any technical aspects.
+
+If you are interested in a functional overview then this is the section for you.
+
+
+## Sequence diagram
+
+![Timelock Sequence Diagram](.docs/timelock-sequence-diagram.png)
---
+++ Timelock/project/timelock-contract/tests/utils/setup.rs
@@ -0,0 +1,40 @@
+use fuels::prelude::*;
+
+// Load abi from json
+abigen!(
+    Timelock,
+    "./project/timelock-contract/out/debug/timelock-contract-abi.json"
+);
+
+
+pub async fn setup() -> (Timelock, WalletUnlocked, WalletUnlocked) {
+    // Launch a local network and deploy the contract
+    let mut wallets = launch_custom_provider_and_get_wallets(
+        WalletsConfig::new(
+            Some(2),             /* Single wallet */
+            Some(1),             /* Single coin (UTXO) */
+            Some(1_000_000_000), /* Amount per coin */
+        ),
+        None,
+        None,
+    )
+    .await;
+
+    let wallet = wallets.pop().unwrap();
+    let wallet2 = wallets.pop().unwrap();
+
+    let id = Contract::deploy(
+        "./out/debug/timelock-contract.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::with_storage_path(Some(
+            "./out/debug/timelock-contract-storage_slots.json".to_string(),
+        )),
+    )
+    .await
+    .unwrap();
+
+    let instance = Timelock::new(id.clone(), wallet.clone());
+
+    (instance, wallet, wallet2)
+}
