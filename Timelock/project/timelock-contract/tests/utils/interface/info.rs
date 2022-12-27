d104b8934843c25a2dcb1fba7adc469007aeca53
Braqzen <sydney.boroch@fuel.sh>
2022-12-27 15:05:51 +0000

    balance, delays utility test methods

--- Timelock/project/timelock-contract/tests/utils/interface/info.rs
+++ Timelock/project/timelock-contract/tests/utils/interface/info.rs
@@ -2,6 +2,14 @@ use fuels::{contract::call_response::FuelCallResponse, prelude::*};
 
 use crate::utils::setup::{ExecutionRange, Timelock};
 
+pub async fn balance(contract: &Timelock, asset_id: ContractId) -> FuelCallResponse<u64> {
+    contract.methods().balance(asset_id).call().await.unwrap()
+}
+
+pub async fn delays(contract: &Timelock) -> FuelCallResponse<(u64, u64)> {
+    contract.methods().delays().call().await.unwrap()
+}
+
 pub async fn queued(contract: &Timelock, id: u64) -> FuelCallResponse<Option<ExecutionRange>> {
     contract.methods().queued(id).call().await.unwrap()
 }
