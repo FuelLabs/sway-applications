38bababa117d370066298bed3dc745ca091c1602
Matt Auer <mattauer@umich.edu>
2023-01-05 17:40:14 -0600

    test: setup basic file

---
+++ auctions/english-auction/packages/app/tests/App.spec.ts
@@ -0,0 +1,8 @@
+import { test } from '@playwright/test';
+
+// TODO figure out how to test with extension
+test.describe('e2e', () => {
+  test('Test auction expires', async () => {});
+
+  test('Test auction is canceled', async () => {});
+});
