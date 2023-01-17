0d4cb932e80e93eda709942ed70a258c3374c39c
Matt Auer <mattauer@umich.edu>
2023-01-17 10:16:52 -0600

    improve form validation

--- auctions/english-auction/packages/app/src/systems/Sell/hooks/useCreateAuctionForm.ts
+++ auctions/english-auction/packages/app/src/systems/Sell/hooks/useCreateAuctionForm.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { yupResolver } from '@hookform/resolvers/yup';
-import { bn, isBech32 } from 'fuels';
+import { bn, DECIMAL_UNITS, isBech32 } from 'fuels';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
@@ -35,6 +35,16 @@ function isValidIdentity(identity: any) {
 function isValidDuration(duration: any) {
   try {
     return !bn(duration).isZero();
+  } catch (e) {
+    return false;
+  }
+}
+
+function isReservePriceValid(reservePrice: any) {
+  try {
+    const bnReservePrice = bn.parseUnits(reservePrice, DECIMAL_UNITS);
+    const bnInitialPrice = bn.parseUnits(this.parent.initialPrice, DECIMAL_UNITS);
+    return bnReservePrice.gt(bnInitialPrice);
   } catch (e) {
     return false;
   }
@@ -75,7 +85,14 @@ const schema = yup
     hasReservePrice: yup.boolean(),
     reservePrice: yup.string().when('hasReservePrice', {
       is: true,
-      then: yup.string().required(),
+      then: yup
+        .string()
+        .test(
+          'is-reserve-price-valid',
+          'Reserve price must be greater than the initial price',
+          isReservePriceValid
+        )
+        .required(),
       otherwise: yup.string().notRequired(),
     }),
     isBidAssetNft: yup.boolean(),
