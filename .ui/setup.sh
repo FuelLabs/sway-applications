835b9eb9f2cd2fda50d5ca1a5bc6c1b8cc6bc9e9
supiket <bengisuozaydin@gmail.com>
2023-01-24 19:32:31 +0300

    workarounds

--- .ui/setup.sh
+++ .ui/setup.sh
@@ -20,6 +20,9 @@ done
 
 [ ${#LINES[@]} != 1 ] || { echo "You must provide at least one Sway contract path"; exit; }
 
+declare -a contract_names
+declare -a contract_ids
+
 for path_index in "${!LINES[@]}"
 do
     [ $path_index != 0 ] || continue
@@ -37,17 +40,26 @@ do
         if [[ $PROJECT_ROOT != *"/project" ]]; then
             PROJECT_ROOT=$(dirname $PROJECT_ROOT)
         fi
-        UI_ROOT=$PROJECT_ROOT/ui
-        env_test=$PROJECT_ROOT/.env.test
-        [ ! -f $env_test ] || { echo "Clearing the contents of $env_test"; > $env_test; }
+        PACKAGES_ROOT=$PROJECT_ROOT/packages
+        UI_ROOT=$PACKAGES_ROOT/app
+
+        # env_test=$PROJECT_ROOT/.env.test
+        # [ ! -f $env_test ] || { echo "Clearing the contents of $env_test"; > $env_test; }
         
+        json=$PROJECT_ROOT/vars.json # replace with env.test
+
         pattern="\"owner\":[[:space:]]\"([a-zA-Z0-9]*)\""
         [[ $(grep owner $SCRIPT_PATH/chainConfig.json) =~ $pattern ]] && wallet="${BASH_REMATCH[1]}"
-        printf "%b" "WALLET_SECRET=\"${wallet}\"\n" >> $env_test
+
+        # printf "%b" "WALLET_SECRET=\"${wallet}\"\n" >> $env_test
+
+        printf "%b" "{\n\t\"walletSecret\": \"${wallet}\"" >> $json
     fi
 
     contract_name=${contract_path##*\/}
-    uppercase_contract_name=$(tr '[:lower:]' '[:upper:]' <<< ${contract_name//-/_})_ID
+    CONTRACT_NAMES+=($contract_name)
+
+    uppercase_underscore_contract_name=$(tr '[:lower:]' '[:upper:]' <<< ${contract_name//-/_})
 
     abi_path="${contract_path}/out/debug/${contract_name}-abi.json"
     [ -f $abi_path ] || { echo "File does not exist $abi_path"; exit; }
@@ -57,37 +69,54 @@ do
     pattern="Contract id:[[:space:]](.*)[[:space:]]contract.*"
     [[ $(forc deploy --url localhost:4000 --unsigned) =~ $pattern ]] && contract_id="${BASH_REMATCH[1]}"
 
-    printf "%b" "${uppercase_contract_name}=\"${contract_id}\"\n" >> $env_test
+    # printf "%b" "${uppercase_underscore_contract_name}_ID=\"${contract_id}\"\n" >> $env_test
+
+    json_contract_name="${contract_name//-/}"
+    json_contract_name="${json_contract_name//contract/Contract}"
+    contract_names+=($json_contract_name)
+    contract_ids+=($contract_id)
 done
 
-if [[ -d $UI_ROOT ]]; then
-    read -e -p "The directory ${MAGENTA}ui${NO_COLOR} already exists. Remove to continue? (y/N) " response
+printf "%b" ",\n\t\"contractNames\": [" >> $json
+for contract_index in "${!contract_names[@]}"
+do
+    if [[ $contract_index != 0 ]]; then
+        printf "%b" ", " >> $json
+    fi
+    printf "%b" "\"${contract_names[$contract_index]}\"" >> $json
+done
+printf "%b" "],\n" >> $json
+
+printf "%b" "\t\"contractIds\": [" >> $json
+for contract_index in "${!contract_ids[@]}"
+do
+    if [[ $contract_index != 0 ]]; then
+        printf "%b" ", " >> $json
+    fi
+    printf "%b" "\"${contract_ids[$contract_index]}\"" >> $json
+done
+printf "%b" "]\n}" >> $json
+
+if [[ -d $PACKAGES_ROOT ]]; then
+    read -e -p "The directory ${MAGENTA}packages${NO_COLOR} already exists. Remove to continue? (y/N) " response
     ([ $response ] && [ $response == "y" ]) || exit
     read -e -p "${RED}THIS IS A DESTRUCTIVE OPERATION.${NO_COLOR} Would you still like to continue? (y/N) " response
     ([ $response ] && [ $response == "y" ]) || exit
-    echo -e "${YELLOW}Removing ${MAGENTA}$UI_ROOT ${YELLOW}and its contents${NO_COLOR}"
-    rm -rf $UI_ROOT
+    echo -e "${YELLOW}Removing ${MAGENTA}$PACKAGES_ROOT ${YELLOW}and its contents${NO_COLOR}"
+    rm -rf $PACKAGES_ROOT
 fi
 
-cd $PROJECT_ROOT
+mkdir $PACKAGES_ROOT
+cd $PACKAGES_ROOT
 
-npx create-react-app ui --template typescript
+npx create-react-app app --template typescript
 
 npm install fuels --save
 
-CONTRACT_TYPES_ROOT="$UI_ROOT/src/contracts"
+echo -e "\n${YELLOW}Generating types for ${PURPLE}${ABI_PATHS[@]}${NO_COLOR}\n" 
+CONTRACT_TYPES_PATH="${CONTRACT_TYPES_ROOT}/$UI_ROOT/src/contracts"
 
-for abi_index in "${!ABI_PATHS[@]}"
-do
-    abi_path=${ABI_PATHS[$abi_index]}
-    echo -e "\n${YELLOW}Generating types for ${PURPLE}$abi_path${NO_COLOR}\n" 
-
-    if [[ ${#ABI_PATHS[@]} > 1 ]]; then
-        CONTRACT_TYPES_PATH="${CONTRACT_TYPES_ROOT}/${CONTRACT_NAMES[$abi_index]}"
-    fi
-
-    npx fuels typegen -i $abi_path -o $CONTRACT_TYPES_PATH
-done
+npx fuels typegen -i ${ABI_PATHS[@]} -o $CONTRACT_TYPES_PATH
 
 cd $UI_ROOT
 
@@ -110,5 +139,9 @@ cat $SCRIPT_PATH/basic/App.css > src/App.css
 cat $SCRIPT_PATH/basic/App.tsx | tail -n +2 > src/App.tsx
 cat $SCRIPT_PATH/basic/index.tsx | tail -n +2 > src/index.tsx
 cat $SCRIPT_PATH/basic/Main.tsx | tail -n +2 > src/Main.tsx
+mv $json src/vars.json
+printf "%b" "/node_modules\n" >> $PACKAGES_ROOT/.gitignore
+
+cp $SCRIPT_PATH/basic/Components.tsx src/Components.tsx
 
 npm start
