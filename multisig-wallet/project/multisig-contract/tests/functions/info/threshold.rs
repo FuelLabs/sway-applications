c1f03cb62d3e794aa84043769a8dba223699c88d
Braqzen <sydney.boroch@fuel.sh>
2023-01-30 18:03:48 +0300

    Additional threshold assertions

--- multisig-wallet/project/multisig-contract/tests/functions/info/threshold.rs
+++ multisig-wallet/project/multisig-contract/tests/functions/info/threshold.rs
@@ -9,10 +9,14 @@ mod success {
     async fn returns_threshold() {
         let (_private_key, deployer, _non_owner) = setup_env(VALID_SIGNER_PK).await.unwrap();
 
+        let initial_threshold = threshold(&deployer.contract).await.value;
+
         constructor(&deployer.contract, default_users()).await;
 
         let current_threshold = threshold(&deployer.contract).await.value;
 
+        assert_eq!(0, initial_threshold);
         assert_eq!(DEFAULT_THRESHOLD, current_threshold);
+        assert_ne!(DEFAULT_THRESHOLD, initial_threshold);
     }
 }
