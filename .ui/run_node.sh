356c40525bf08d463b8ebf6c073c1e031db424dd
supiket <bengisuozaydin@gmail.com>
2023-01-31 03:40:28 +0300

    working script with basic and missing ui

--- .ui/run_node.sh
+++ .ui/run_node.sh
@@ -8,4 +8,5 @@ SCRIPT_ROOT=$(dirname $(realpath $0))
 cd $SCRIPT_ROOT
 
 echo -e "${YELLOW}Starting local Fuel node${NO_COLOR}"
+
 fuel-core run --ip 127.0.0.1 --port 4000 --chain ./chainConfig.json --db-type in-memory
--- .ui/setup.sh
+++ .ui/setup.sh
@@ -1,147 +1,141 @@
 #!/bin/bash
 
-MAGENTA=`tput setaf 5`
-NO_COLOR=`tput sgr0`
-RED=`tput setaf 1`
-YELLOW=`tput setaf 3`
+SCRIPT_PATH=$(dirname $(realpath $0)) # path: sway-applications/.ui
+REPO_ROOT=$(dirname $SCRIPT_PATH) # path: sway-applications
 
-declare -a LINES
-declare -a CONTRACT_NAMES
-declare -a ABI_PATHS
+# colors used for some messages written to standard output
+MAGENTA=`tput setaf 5` # used for paths
+NO_COLOR=`tput sgr0` # used to reset to default color
+RED=`tput setaf 1` # used for destructive operations
+YELLOW=`tput setaf 3` # used for purely informative messages
 
-SCRIPT_PATH=$(dirname $(realpath $0))
-REPO_ROOT=$(dirname $SCRIPT_PATH)
+# declare arrays
+declare -a contract_project_paths # array of contract project paths relative to REPO_ROOT, e.g. AMM/project/contracts/exchange-contract
+declare -a abi_paths # array of real paths of generated ABIs, e.g. sway-applications/AMM/project/contracts/exchange-contract/out/exchange-contract-abi.json
 
-while IFS= read -e -p "Enter contract project path: ${MAGENTA}sway-applications/${NO_COLOR}" line
+# read standard input to get contract projects
+# TODO: change to expect abi.json paths instead
+while IFS= read -e -p "Enter contract project path: ${MAGENTA}sway-applications/${NO_COLOR}" input_path
 do 
-    [ $line ] || break
-    LINES+=("$line")
+    [ $input_path ] || break
+    contract_project_paths+=("$input_path")
 done
 
-[ ${#LINES[@]} != 1 ] || { echo "You must provide at least one Sway contract path"; exit; }
+# check that at least one path has been inputted
+[ ${#contract_project_paths[@]} != 0 ] || { echo "You must provide at least one Sway contract project path"; exit; }
 
-declare -a contract_names
-declare -a contract_ids
-
-for path_index in "${!LINES[@]}"
+# iterate all provided contract project paths
+for path_index in "${!contract_project_paths[@]}"
 do
-    [ $path_index != 0 ] || continue
+    # real path of contract project
+    contract_path=$REPO_ROOT/${contract_project_paths[$path_index]}
 
-    contract_path=$REPO_ROOT/${LINES[$path_index]}
-
+    # remove trailing slash if it exists, to avoid duplicate slashes and to be able to extract contract name
     if [[ $contract_path == *"/" ]]; then
         contract_path=${contract_path%?}
     fi
 
-    [ -d "$contract_path/" ] || { echo "Directory does not exist $contract_path"; exit; }
+    # check that directory exists
+    [ -d "$contract_path" ] || { echo "Directory does not exist $contract_path"; exit; }
 
-    if [[ $path_index == 1 ]]; then
-        PROJECT_ROOT=$(dirname ${contract_path})
-        if [[ $PROJECT_ROOT != *"/project" ]]; then
-            PROJECT_ROOT=$(dirname $PROJECT_ROOT)
-        fi
-        PACKAGES_ROOT=$PROJECT_ROOT/packages
-        UI_ROOT=$PACKAGES_ROOT/app
-
-        # env_test=$PROJECT_ROOT/.env.test
-        # [ ! -f $env_test ] || { echo "Clearing the contents of $env_test"; > $env_test; }
-        
-        json=$PROJECT_ROOT/vars.json # replace with env.test
-
-        pattern="\"owner\":[[:space:]]\"([a-zA-Z0-9]*)\""
-        [[ $(grep owner $SCRIPT_PATH/chainConfig.json) =~ $pattern ]] && wallet="${BASH_REMATCH[1]}"
-
-        # printf "%b" "WALLET_SECRET=\"${wallet}\"\n" >> $env_test
-
-        printf "%b" "{\n\t\"walletSecret\": \"${wallet}\"" >> $json
-    fi
-
+    # contract name is the part of contract path that is before the last "/", e.g. exchange-contract
     contract_name=${contract_path##*\/}
-    CONTRACT_NAMES+=($contract_name)
 
-    uppercase_underscore_contract_name=$(tr '[:lower:]' '[:upper:]' <<< ${contract_name//-/_})
-
+    # real path of generated abi file, e.g. sway-applications/AMM/project/contracts/exchange-contract/out/debug/exchange-contract-abi.json 
     abi_path="${contract_path}/out/debug/${contract_name}-abi.json"
     [ -f $abi_path ] || { echo "File does not exist $abi_path"; exit; }
-    ABI_PATHS+=($abi_path)
+    abi_paths+=($abi_path)
 
     cd $contract_path
     pattern="Contract id:[[:space:]](.*)[[:space:]]contract.*"
+    # deploy contract and extract the contract id from standard output
     [[ $(forc deploy --url localhost:4000 --unsigned) =~ $pattern ]] && contract_id="${BASH_REMATCH[1]}"
 
-    # printf "%b" "${uppercase_underscore_contract_name}_ID=\"${contract_id}\"\n" >> $env_test
+    # operations for the first iteration
+    # assumes that the application is structured as app/project/contracts/<contract> or app/project/<contract>
+    if [[ $path_index == 0 ]]; then
+        # real path of project root, e.g. sway-applications/AMM/project or sway-applications/auctions/english-auction/project
+        project_path=$(dirname ${contract_path})
+        if [[ $project_path != *"/project" ]]; then
+            project_path=$(dirname $project_path)
+        fi
 
-    json_contract_name="${contract_name//-/}"
-    json_contract_name="${json_contract_name//contract/Contract}"
-    contract_names+=($json_contract_name)
-    contract_ids+=($contract_id)
-done
+        # real path of packages directory that will be created, e.g. sway-applications/AMM/project/packages
+        packages_path=$project_path/packages
+        # real path of the frontend application that will be created, e.g. sway-applications/AMM/project/packages/app
+        ui_path=$packages_path/app
 
-printf "%b" ",\n\t\"contractNames\": [" >> $json
-for contract_index in "${!contract_names[@]}"
-do
-    if [[ $contract_index != 0 ]]; then
-        printf "%b" ", " >> $json
+        # file that will contain deployed contract ids
+        # TODO: replace with env.test with wallet private keys
+        json=$project_path/vars.json
+
+        # write the private key of the first wallet in .ui/chainConfig.json to vars.json
+        # TODO: remove once environment variables and the wallet SDK are integrated
+        printf "%b" "{\n\t\"walletSecret\": \"0xa449b1ffee0e2205fa924c6740cc48b3b473aa28587df6dab12abc245d1f5298\"," >> $json
+        printf "%b" "\n\t\"contractIds\": [" >> $json
     fi
-    printf "%b" "\"${contract_names[$contract_index]}\"" >> $json
-done
-printf "%b" "],\n" >> $json
 
-printf "%b" "\t\"contractIds\": [" >> $json
-for contract_index in "${!contract_ids[@]}"
-do
-    if [[ $contract_index != 0 ]]; then
+    # write the deployed contract ids to vars.json
+    printf "%b" "\"${contract_id}\"" >> $json
+    if [[ $path_index != $((${#contract_project_paths[@]} - 1)) ]]; then
         printf "%b" ", " >> $json
+    else
+        printf "%b" "]\n}" >> $json
     fi
-    printf "%b" "\"${contract_ids[$contract_index]}\"" >> $json
 done
-printf "%b" "]\n}" >> $json
 
-if [[ -d $PACKAGES_ROOT ]]; then
-    read -e -p "The directory ${MAGENTA}packages${NO_COLOR} already exists. Remove to continue? (y/N) " response
+# check if the packages directory already exists and remove it after confirming twice, in order to avoid collisions
+if [[ -d $packages_path ]]; then
+    read -e -p "The directory ${MAGENTA}$packages_path${NO_COLOR} already exists. Remove to continue? (y/N) " response
     ([ $response ] && [ $response == "y" ]) || exit
     read -e -p "${RED}THIS IS A DESTRUCTIVE OPERATION.${NO_COLOR} Would you still like to continue? (y/N) " response
     ([ $response ] && [ $response == "y" ]) || exit
-    echo -e "${YELLOW}Removing ${MAGENTA}$PACKAGES_ROOT ${YELLOW}and its contents${NO_COLOR}"
-    rm -rf $PACKAGES_ROOT
+    echo -e "${YELLOW}Removing ${MAGENTA}$packages_path${YELLOW} and its contents${NO_COLOR}"
+    rm -rf $packages_path
 fi
 
-mkdir $PACKAGES_ROOT
-cd $PACKAGES_ROOT
+mkdir $packages_path
+cd $packages_path
 
+# create react app with the typescript template
 npx create-react-app app --template typescript
 
+# install fuels
 npm install fuels --save
 
-echo -e "\n${YELLOW}Generating types for ${PURPLE}${ABI_PATHS[@]}${NO_COLOR}\n" 
-CONTRACT_TYPES_PATH="${CONTRACT_TYPES_ROOT}/$UI_ROOT/src/contracts"
+# generate types for contracts
+echo -e "\n${YELLOW}Generating types for ${PURPLE}${abi_paths[@]}${NO_COLOR}\n" 
+npx fuels typegen -i ${abi_paths[@]} -o $ui_path/src/contracts
 
-npx fuels typegen -i ${ABI_PATHS[@]} -o $CONTRACT_TYPES_PATH
+cd $ui_path
 
-cd $UI_ROOT
+# remove unnecessary files
+rm public/favicon.ico
+rm public/logo192.png
+rm public/logo512.png
+rm public/manifest.json
+rm public/robots.txt
+rm src/App.test.tsx
+rm src/logo.svg
+rm src/react-app-env.d.ts
+rm src/reportWebVitals.ts
+rm src/setupTests.ts
 
-rm -v public/favicon.ico
-rm -v public/logo192.png
-rm -v public/logo512.png
-rm -v public/manifest.json
-rm -v public/robots.txt
-rm -v src/App.test.tsx
-rm -v src/logo.svg
-rm -v src/react-app-env.d.ts
-rm -v src/reportWebVitals.ts
-rm -v src/setupTests.ts
+# replace contents of some files in the src directory
 > public/index.html
 > src/App.css
 > src/App.tsx
 > src/index.tsx
-cat $SCRIPT_PATH/basic/index.html > public/index.html
-cat $SCRIPT_PATH/basic/App.css > src/App.css
-cat $SCRIPT_PATH/basic/App.tsx | tail -n +2 > src/App.tsx
-cat $SCRIPT_PATH/basic/index.tsx | tail -n +2 > src/index.tsx
-cat $SCRIPT_PATH/basic/Main.tsx | tail -n +2 > src/Main.tsx
+cat $SCRIPT_PATH/src/index.html > public/index.html
+cat $SCRIPT_PATH/src/App.css > src/App.css
+cat $SCRIPT_PATH/src/App.tsx | tail -n +2 > src/App.tsx
+cat $SCRIPT_PATH/src/index.tsx | tail -n +2 > src/index.tsx
+
+# add missing files to the src directory
+cat $SCRIPT_PATH/src/Components.tsx | tail -n +2 > src/Components.tsx
+cat $SCRIPT_PATH/src/Main.tsx | tail -n +2 > src/Main.tsx
 mv $json src/vars.json
-printf "%b" "/node_modules\n" >> $PACKAGES_ROOT/.gitignore
 
-cp $SCRIPT_PATH/basic/Components.tsx src/Components.tsx
-
-npm start
+# you can run `npm start` in $ui_path, e.g. AMM/project/packages/app
+# or uncomment the next line before running this script. it will clear the previous standard outputs by this script
+# npm start
