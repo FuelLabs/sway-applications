54c211cd2a65ec6d029ec994dc4b02b00649a0c4
Matt Auer <mattauer@umich.edu>
2023-01-31 08:55:21 -0600

    add more logs

--- auctions/english-auction/packages/app/src/systems/Buy/hooks/useBid.ts
+++ auctions/english-auction/packages/app/src/systems/Buy/hooks/useBid.ts
@@ -21,6 +21,7 @@ export const useBid = ({ auctionId, auctionAsset, setAssetAmount }: UseBidProps)
     async () => {
       if (!contract) throw new Error('Contract not connected');
       const callParams: CoinQuantityLike | undefined = auctionAsset.TokenAsset ?? undefined;
+      console.log('auction asset: ', auctionAsset);
       const { transactionResult } = auctionAsset.NFTAsset
         ? await contract.functions
             .bid(auctionId, auctionAsset)
