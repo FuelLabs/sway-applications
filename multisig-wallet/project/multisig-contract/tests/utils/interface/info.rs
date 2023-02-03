127ca0669bb8b511818a8dfc751666b9d1450d0f
Kyran Rawlinson <kyranrawlinson@gmail.com>
2023-02-03 18:57:24 +0000

    Updated info test

--- multisig-wallet/project/multisig-contract/tests/utils/interface/info.rs
+++ multisig-wallet/project/multisig-contract/tests/utils/interface/info.rs
@@ -1,4 +1,8 @@
-use fuels::{programs::call_response::FuelCallResponse, tx::ContractId, types::Bits256};
+use fuels::{
+    programs::call_response::FuelCallResponse,
+    tx::ContractId,
+    types::{Bits256, Identity},
+};
 
 use crate::utils::setup::{MultiSig, TypeToHash};
 
@@ -14,13 +18,43 @@ pub async fn threshold(contract: &MultiSig) -> FuelCallResponse<u64> {
     contract.methods().threshold().call().await.unwrap()
 }
 
-pub async fn calculate_hash(
+pub async fn compute_hash(
     contract: &MultiSig,
     type_to_hash: TypeToHash,
 ) -> FuelCallResponse<Bits256> {
     contract
         .methods()
-        .calculate_hash(type_to_hash)
+        .compute_hash(type_to_hash)
+        .call()
+        .await
+        .unwrap()
+}
+
+pub async fn compute_transaction_hash(
+    contract: &MultiSig,
+    contract_identifier: ContractId,
+    nonce: u64,
+    value: Option<u64>,
+    asset_id: Option<ContractId>,
+    target: Identity,
+    function_selector: Option<Bytes>,
+    calldata: Option<Vec<u8>>,
+    single_value_type_arg: Option<bool>,
+    forwarded_gas: Option<u64>,
+) -> FuelCallResponse<Bits256> {
+    contract
+        .methods()
+        .compute_transaction_hash(
+            contract_identifier,
+            nonce,
+            value,
+            asset_id,
+            target,
+            function_selector,
+            calldata,
+            single_value_type_arg,
+            forwarded_gas,
+        )
         .call()
         .await
         .unwrap()
