a1272c0f478afefafdd39b49b298ce0af0ab3d0a
Kyran Rawlinson <kyranrawlinson@gmail.com>
2023-02-03 17:23:47 +0000

    Setup testing for core

--- multisig-wallet/project/multisig-contract/tests/functions/core/execute_transaction.rs
+++ multisig-wallet/project/multisig-contract/tests/functions/core/execute_transaction.rs
@@ -2,9 +2,15 @@ mod success {
 
     #[ignore]
     #[tokio::test]
-    async fn executes_transaction() {
-        // Currently ignored and unimplemented, awaiting the ability to implement https://github.com/FuelLabs/sway-applications/issues/22
-    }
+    async fn executes_transfer() {}
+
+    #[ignore]
+    #[tokio::test]
+    async fn executes_call_without_value() {}
+
+    #[ignore]
+    #[tokio::test]
+    async fn executes_call_with_value() {}
 }
 
 mod revert {
@@ -12,28 +18,61 @@ mod revert {
     #[ignore]
     #[tokio::test]
     #[should_panic(expected = "NotInitialized")]
-    async fn not_initialized() {
-        // Currently ignored and unimplemented, awaiting the ability to implement https://github.com/FuelLabs/sway-applications/issues/22
+    async fn not_initialized() {}
+
+    mod transfer {
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "TransferRequiresAnAssetId")]
+        async fn transfer_requires_an_asset_id() {}
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "TransferRequiresAValue")]
+        async fn transfer_requires_a_value() {}
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "InsufficientAssetAmount")]
+        async fn insufficient_asset_amount() {}
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "InsufficientApprovals")]
+        async fn insufficient_approvals() {}
     }
 
-    #[ignore]
-    #[tokio::test]
-    #[should_panic(expected = "InsufficientAssetAmount")]
-    async fn insufficient_asset_amount() {
-        // Currently ignored and unimplemented, awaiting the ability to implement https://github.com/FuelLabs/sway-applications/issues/22
-    }
+    mod call {
 
-    #[ignore]
-    #[tokio::test]
-    #[should_panic(expected = "IncorrectSignerOrdering")]
-    async fn incorrect_signer_ordering() {
-        // Currently ignored and unimplemented, awaiting the ability to implement https://github.com/FuelLabs/sway-applications/issues/22
-    }
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "CannotCallFunctionsOnAddresses")]
+        async fn cannot_call_functions_on_addresses() {}
 
-    #[ignore]
-    #[tokio::test]
-    #[should_panic(expected = "InsufficientApprovals")]
-    async fn insufficient_approvals() {
-        // Currently ignored and unimplemented, awaiting the ability to implement https://github.com/FuelLabs/sway-applications/issues/22
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "CallingFunctionsRequiresCalldata")]
+        async fn calling_functions_requires_calldata() {}
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "CallingFunctionsRequiresSingleValueTypeArg")]
+        async fn calling_functions_requires_single_value_type_arg() {}
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "TransferRequiresAnAssetId")]
+        async fn transfer_requires_an_asset_id() {}
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "InsufficientAssetAmount")]
+        async fn insufficient_asset_amount() {}
+
+        #[ignore]
+        #[tokio::test]
+        #[should_panic(expected = "InsufficientApprovals")]
+        async fn insufficient_approvals() {}
     }
 }
