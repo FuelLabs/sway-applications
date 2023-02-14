71f7025560812f0976ed82159ccbfd7c47801bc1
supiket <bengisuozaydin@gmail.com>
2023-02-14 14:23:32 -0700

    add `fuel.connect()` to `useProvider`

--- OTC-swap-predicate/frontend/src/hooks/useProvider.ts
+++ OTC-swap-predicate/frontend/src/hooks/useProvider.ts
@@ -3,26 +3,30 @@ import { useQuery } from "@tanstack/react-query";
 import useFuel from "./useFuel";
 
 const useProvider = () => {
-  const [fuel] = useFuel();
+    const [fuel] = useFuel();
 
-  if (!fuel) toast.error("Error fuelWeb3 instance is not defined");
+    if (!fuel) toast.error("Error fuelWeb3 instance is not defined");
 
-  const {
-    data: provider,
-    isLoading,
-    isError,
-  } = useQuery(
-    ["provider"],
-    async () => {
-        const fuelProvider = await fuel.getProvider();
-        return fuelProvider;
-    },
-    {
-      enabled: !!fuel,
-    }
-  );
+    const {
+        data: provider,
+        isLoading,
+        isError,
+    } = useQuery(
+        ["provider"],
+        async () => {
+            const isConnected = await fuel.isConnected();
+            if (!isConnected) {
+                await fuel.connect();
+            }
+            const fuelProvider = await fuel.getProvider();
+            return fuelProvider;
+        },
+        {
+            enabled: !!fuel,
+        }
+    );
 
-  return { provider, isLoading, isError };
+    return { provider, isLoading, isError };
 };
 
 export default useProvider;
