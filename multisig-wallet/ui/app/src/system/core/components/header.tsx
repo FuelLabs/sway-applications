bab34b8fce6f6569d3a6929126322d943d8a5b12
Braqzen <sydney.boroch@fuel.sh>
2023-02-17 16:20:28 +0300

    tmp css hack

--- multisig-wallet/ui/app/src/system/core/components/header.tsx
+++ multisig-wallet/ui/app/src/system/core/components/header.tsx
@@ -8,18 +8,18 @@ export const Header = () => {
     }
 
     return (
-        <Flex css={{ boxShadow: "0px 2px black" }}>
-            <BoxCentered css={{ background: 'rgb(63 149 57)', justifyContent: 'flex-start' }}>
+        <Flex>
+            <BoxCentered css={{ background: 'rgb(63 149 57)', justifyContent: 'flex-start', boxShadow: "11px 1px 8px 0px black", borderBottom: "1px solid black" }}>
                 <FuelLogo />
             </BoxCentered>
 
-            <BoxCentered css={{ background: 'rgb(63 149 57)', width: '60%', justifyContent: 'flex-start' }}>
+            <BoxCentered css={{ background: 'rgb(63 149 57)', width: '60%', justifyContent: 'flex-start', boxShadow: "11px 1px 8px 0px black", borderBottom: "1px solid black" }}>
                 <Heading as="h3" css={{ marginTop: 'auto', marginBottom: "auto", textShadow: '-1px -1px 0 #000, 0 -1px 0 #000, 1px -1px 0 #000, 1px 0 0 #000, 1px  1px 0 #000, 0 1px 0 #000, -1px 1px 0 #000, -1px 0 0 #000'}}>
                     Multi-Signature Wallet
                 </Heading>
             </BoxCentered>
 
-            <BoxCentered css={{ background: 'rgb(63 149 57)', width: '40%', justifyContent: 'space-evenly' }}>
+            <BoxCentered css={{ background: 'rgb(63 149 57)', width: '40%', justifyContent: 'space-evenly', boxShadow: "11px 1px 8px 0px black", borderBottom: "1px solid black" }}>
                 <ButtonLink href="#" onClick={doNothing} css={{ color: 'black', fontWeight: 'bolder' }}>
                     Create
                 </ButtonLink>
