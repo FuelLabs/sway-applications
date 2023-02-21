508434dabbda777ca4bb1edba250f616f790b821
Braqzen <sydney.boroch@fuel.sh>
2023-02-21 22:26:20 +0300

    timelock contract

---
+++ timelock/project/timelock-contract/tests/functions/info/queued.rs
@@ -0,0 +1,14 @@
+mod success {
+
+    crate::utils::{
+        interface::{core::queue, info::{queued, transaction_hash}},
+        setup::setup,
+    };
+
+    #[tokio::test]
+    async fn returns_none() {}
+
+    #[tokio::test]
+    async fn returns_info() {}
+
+}
