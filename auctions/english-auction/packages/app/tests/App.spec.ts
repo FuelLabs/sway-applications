c89798b7d10788bb8f7d3d2fc8062b6d8a25740f
Matt Auer <mattauer@umich.edu>
2023-02-23 12:28:51 -0600

    chore: more logs

--- auctions/english-auction/packages/app/tests/App.spec.ts
+++ auctions/english-auction/packages/app/tests/App.spec.ts
@@ -64,6 +64,7 @@ async function walletSetup(context: BrowserContext, extensionId: string) {
   await walletPage.reload({ waitUntil: 'load' });
   await walletPage.locator('[aria-label="Network name"]').fill('test');
   console.log("url: ", process.env.VITE_FUEL_PROVIDER_URL);
+  console.log("env: ", process.env);
   await walletPage.locator('[aria-label="Network URL"]').fill(process.env.VITE_FUEL_PROVIDER_URL!);
   await walletPage.locator('button', { hasText: 'Create' }).click();
 
