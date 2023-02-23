a89ae58b33af586ef1d86648601a70b8e1c69533
Matt Auer <mattauer@umich.edu>
2023-02-22 21:04:55 -0600

    Ci: debugging

--- auctions/english-auction/packages/app/tests/App.spec.ts
+++ auctions/english-auction/packages/app/tests/App.spec.ts
@@ -20,6 +20,8 @@ async function walletSetup(context: BrowserContext, extensionId: string) {
 
   // WALLET SETUP
   const walletPage = await context.newPage();
+  appPage.on('console', (msg) => console.log(msg));
+  walletPage.on('console', (msg) => console.log(msg));
   await walletPage.goto(`chrome-extension://${extensionId}/popup.html`);
   const signupPage = await context.waitForEvent('page', {
     predicate: (page) => page.url().includes('sign-up'),
