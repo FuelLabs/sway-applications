edd4b63b794b37eb02d8f17eeb6c7ad2a371b3bb
Braqzen <103777923+Braqzen@users.noreply.github.com>
2023-02-24 02:01:10 +0300

    Escrow: storage getters

--- escrow/project/contracts/escrow-contract/tests/utils/mod.rs
+++ escrow/project/contracts/escrow-contract/tests/utils/mod.rs
@@ -193,6 +193,46 @@ pub(crate) mod abi_calls {
             .await
             .unwrap()
     }
+
+    pub(crate) async fn arbiter_proposal(contract: &Escrow, identifier: u64) -> Option<Arbiter> {
+        contract
+            .methods()
+            .arbiter_proposal(identifier)
+            .call()
+            .await
+            .unwrap()
+            .value
+    }
+
+    pub(crate) async fn assets(contract: &Escrow, identifier: u64) -> Option<Asset> {
+        contract
+            .methods()
+            .assets(identifier)
+            .call()
+            .await
+            .unwrap()
+            .value
+    }
+
+    pub(crate) async fn escrows(contract: &Escrow, identifier: u64) -> Option<EscrowInfo> {
+        contract
+            .methods()
+            .escrows(identifier)
+            .call()
+            .await
+            .unwrap()
+            .value
+    }
+
+    pub(crate) async fn escrow_count(contract: &Escrow) -> u64 {
+        contract
+            .methods()
+            .escrow_count()
+            .call()
+            .await
+            .unwrap()
+            .value
+    }
 }
 
 pub(crate) mod test_helpers {
