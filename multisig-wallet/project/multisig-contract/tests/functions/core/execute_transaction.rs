baa86185a1ee40e715904ce8e06b19f7a345dfc1
Kyran Rawlinson <kyranrawlinson@gmail.com>
2023-02-24 17:23:13 +0000

    TEST: executes_transfer

--- multisig-wallet/project/multisig-contract/tests/functions/core/execute_transaction.rs
+++ multisig-wallet/project/multisig-contract/tests/functions/core/execute_transaction.rs
@@ -1,8 +1,123 @@
+use crate::utils::{
+    interface::{
+        core::{constructor, execute_transaction},
+        info::{compute_transaction_hash, nonce},
+    },
+    setup::{
+        base_asset_contract_id, compute_signatures, default_users, setup_env, transfer_parameters,
+        DEFAULT_TRANSFER_AMOUNT, VALID_SIGNER_PK,
+    },
+};
+use fuels::{
+    prelude::{TxParameters, BASE_ASSET_ID},
+    signers::fuel_crypto::Message,
+};
+
 mod success {
 
-    #[ignore]
+    use super::*;
+    use crate::utils::{interface::info::balance, setup::TransferEvent};
+
     #[tokio::test]
-    async fn executes_transfer() {}
+    async fn executes_transfer() {
+        let (private_key, deployer, _non_owner) = setup_env(VALID_SIGNER_PK).await.unwrap();
+
+        constructor(&deployer.contract, default_users()).await;
+
+        let initial_nonce = nonce(&deployer.contract).await.value;
+
+        let (receiver_wallet, tx) = transfer_parameters(&deployer, initial_nonce).await;
+
+        deployer
+            .wallet
+            .force_transfer_to_contract(
+                deployer.contract.contract_id(),
+                DEFAULT_TRANSFER_AMOUNT,
+                BASE_ASSET_ID,
+                TxParameters::default(),
+            )
+            .await
+            .unwrap();
+
+        // Check balances pre-transfer
+        let initial_contract_balance = balance(&deployer.contract, base_asset_contract_id())
+            .await
+            .value;
+
+        let initial_receiver_balance = deployer
+            .wallet
+            .get_provider()
+            .unwrap()
+            .get_asset_balance(receiver_wallet.address(), BASE_ASSET_ID)
+            .await
+            .unwrap();
+
+        let tx_hash = compute_transaction_hash(
+            &deployer.contract,
+            tx.contract_identifier,
+            tx.nonce,
+            tx.value,
+            tx.asset_id,
+            tx.target.clone(),
+            tx.function_selector.clone(),
+            tx.calldata.clone(),
+            tx.single_value_type_arg,
+            tx.forwarded_gas,
+        )
+        .await
+        .value
+        .0;
+        let tx_hash = unsafe { Message::from_bytes_unchecked(tx_hash) };
+
+        let signatures = compute_signatures(private_key, tx_hash).await;
+
+        let response = execute_transaction(
+            &deployer.contract,
+            tx.asset_id,
+            tx.calldata,
+            tx.forwarded_gas,
+            tx.function_selector,
+            signatures,
+            tx.single_value_type_arg,
+            tx.target.clone(),
+            tx.value,
+        )
+        .await;
+
+        let log = response.get_logs_with_type::<TransferEvent>().unwrap();
+        let event = log.get(0).unwrap();
+        assert_eq!(
+            *event,
+            TransferEvent {
+                asset: tx.asset_id.unwrap(),
+                nonce: tx.nonce,
+                target: tx.target,
+                value: tx.value.unwrap(),
+            }
+        );
+
+        // check balances post-transfer
+        let final_contract_balance = balance(&deployer.contract, base_asset_contract_id())
+            .await
+            .value;
+
+        let final_receiver_balance = deployer
+            .wallet
+            .get_provider()
+            .unwrap()
+            .get_asset_balance(receiver_wallet.address(), BASE_ASSET_ID)
+            .await
+            .unwrap();
+
+        assert_eq!(initial_contract_balance, DEFAULT_TRANSFER_AMOUNT);
+        assert_eq!(initial_receiver_balance, 0);
+
+        assert_eq!(final_contract_balance, 0);
+        assert_eq!(final_receiver_balance, DEFAULT_TRANSFER_AMOUNT);
+
+        assert!(final_contract_balance < initial_contract_balance);
+        assert!(final_receiver_balance > initial_receiver_balance);
+    }
 
     #[ignore]
     #[tokio::test]
