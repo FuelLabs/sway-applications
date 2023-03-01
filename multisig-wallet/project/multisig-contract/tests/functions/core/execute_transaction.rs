a09324820b8afc5cd06b90d3dd806ae7a8141784
Kyran Rawlinson <kyranrawlinson@gmail.com>
2023-03-01 21:18:55 +0000

    TEST: call-InsufficientApprovals

--- multisig-wallet/project/multisig-contract/tests/functions/core/execute_transaction.rs
+++ multisig-wallet/project/multisig-contract/tests/functions/core/execute_transaction.rs
@@ -844,9 +844,63 @@ mod revert {
             .unwrap();
         }
 
-        #[ignore]
         #[tokio::test]
         #[should_panic(expected = "InsufficientApprovals")]
-        async fn insufficient_approvals() {}
+        async fn insufficient_approvals() {
+            let (private_key, deployer, _non_owner) = setup_env(VALID_SIGNER_PK).await.unwrap();
+
+            constructor(&deployer.contract, default_users()).await;
+
+            let initial_nonce = nonce(&deployer.contract).await.value;
+
+            deployer
+                .wallet
+                .force_transfer_to_contract(
+                    deployer.contract.contract_id(),
+                    DEFAULT_TRANSFER_AMOUNT,
+                    BASE_ASSET_ID,
+                    TxParameters::default(),
+                )
+                .await
+                .unwrap();
+
+            let test_contract = deploy_test_contract(deployer.wallet.clone()).await.unwrap();
+
+            let tx = call_parameters(&deployer, initial_nonce, &test_contract, true).await;
+
+            let tx_hash = compute_transaction_hash(
+                &deployer.contract,
+                tx.contract_identifier,
+                tx.nonce,
+                tx.value,
+                tx.asset_id,
+                tx.target.clone(),
+                tx.function_selector.clone(),
+                tx.calldata.clone(),
+                tx.single_value_type_arg,
+                tx.forwarded_gas,
+            )
+            .await
+            .value
+            .0;
+            let tx_hash = unsafe { Message::from_bytes_unchecked(tx_hash) };
+
+            let mut signatures = compute_signatures(private_key, tx_hash).await;
+            signatures.pop();
+
+            execute_transaction(
+                &deployer.contract,
+                tx.asset_id,
+                tx.calldata.clone(),
+                tx.forwarded_gas,
+                tx.function_selector.clone(),
+                signatures,
+                tx.single_value_type_arg,
+                tx.target.clone(),
+                tx.value,
+            )
+            .await
+            .unwrap();
+        }
     }
 }
