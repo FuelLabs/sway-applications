ed30ef3177fba7c53a6e05959c4d135e6ba19dd4
Braqzen <sydney.boroch@fuel.sh>
2023-03-04 23:19:53 +0000

    Threshold page

--- multisig-wallet/ui/app/src/system/threshold/pages/threshold.tsx
+++ multisig-wallet/ui/app/src/system/threshold/pages/threshold.tsx
@@ -1,9 +1,7 @@
-import { BoxCentered, Button, Heading, Input, toast, Stack } from "@fuel-ui/react";
-import { useState } from "react";
+import { BoxCentered, Button, Flex, Heading, Input, toast, Stack } from "@fuel-ui/react";
 import { useContract } from "../../core/hooks";
 
 export function ThresholdPage() {
-    const [threshold, setThreshold] = useState("N/A")
     const { contract, isLoading, isError } = useContract()
 
     async function useThreshold() {
@@ -24,26 +22,110 @@ export function ThresholdPage() {
 
     async function getThreshold() {
         const { value } = await contract!.functions.threshold().get();
-        setThreshold(String(value));
+        toast.success(`Threshold: ${value}`, { duration: 10000 });
+    }
+
+    async function getHash() {
+        const data = document.querySelector<HTMLInputElement>(
+            `[name="threshold-hash-data"]`
+        )!.value;
+
+        const nonce = document.querySelector<HTMLInputElement>(
+            `[name="threshold-hash-nonce"]`
+        )!.value;
+
+        let threshold = document.querySelector<HTMLInputElement>(
+            `[name="threshold-hash"]`
+        )!.value;
+
+        // TODO: merge in new hashing function and use instead of this incorrect one
+        const { value } = await contract!.functions.threshold_hash(data, nonce, threshold).get();
+        toast.success(`Hash: ${value}`, { duration: 10000 });
+    }
+
+    async function getNonce() {
+        const { value } = await contract!.functions.nonce().get();
+        toast.success(`Current nonce: ${value}`, { duration: 10000 });
     }
 
     return (
-        <BoxCentered css={{ marginTop: "15%", width: "30%" }}>
+        <BoxCentered css={{ marginTop: "8%" }}>
 
             <Stack gap="$1">
 
-                <Heading as="h3" css={{ marginBottom: "15%", color: "$accent1"}}>
+                <Flex gap="$24" css={{ marginBottom: "$14" }}>
+
+                    <Stack>
+
+                        <Heading as="h4" css={{ marginLeft: "auto", marginRight: "auto", color: "$accent1" }}>
+                            Check current nonce
+                        </Heading>
+
+                        <Button
+                            color="accent"
+                            onPress={getNonce}
+                            size="lg"
+                            variant="solid"
+                            css={{ marginBottom: "$2" }}
+                        >
+                            Get nonce
+                        </Button>
+
+                        <Heading as="h4" css={{ marginLeft: "auto", marginRight: "auto", marginTop: "$14", color: "$accent1" }}>
+                            Check current threshold
+                        </Heading>
+
+                        <Button
+                            color="accent"
+                            onPress={getThreshold}
+                            size="lg"
+                            variant="solid"
+                        >
+                            Get threshold
+                        </Button>
+
+                    </Stack>
+
+                    <Stack>
+
+                        <Heading as="h4" css={{ marginLeft: "auto", marginRight: "auto", color: "$accent1" }}>
+                            Create transaction hash to sign
+                        </Heading>
+
+                        <Input size="lg">
+                            <Input.Field name="threshold-hash-data" placeholder="Optional data" />
+                        </Input>
+                        <Input size="lg">
+                            <Input.Number name="threshold-hash-nonce" placeholder="Nonce" />
+                        </Input>
+                        <Input size="lg">
+                            <Input.Number name="threshold-hash" placeholder="New threshold" />
+                        </Input>
+                        <Button
+                            color="accent"
+                            onPress={getHash}
+                            size="lg"
+                            variant="solid"
+                        >
+                            Create hash
+                        </Button>
+
+                    </Stack>
+
+                </Flex>
+
+                <Heading as="h3" css={{ marginLeft: "auto", marginRight: "auto", marginBottom: "$10", color: "$accent1"}}>
                     Change the threshold for execution
                 </Heading>
 
                 <Input size="lg">
-                    <Input.Field name="threshold-data" placeholder="Optional data"/>
+                    <Input.Field name="threshold-data" placeholder="Optional data" />
                 </Input>
                 <Input size="lg">
-                    <Input.Field name="threshold-signature" placeholder="Signature"/>
+                    <Input.Field name="threshold-signature" placeholder="Signature" />
                 </Input>
                 <Input size="lg">
-                    <Input.Number name="threshold" placeholder="Threshold"/>
+                    <Input.Number name="threshold" placeholder="Threshold" />
                 </Input>
                 <Button
                     color="accent"
@@ -52,19 +134,6 @@ export function ThresholdPage() {
                     variant="solid"
                 >
                     Set threshold
-                </Button>
-
-                <Heading as="h4" css={{ marginLeft: "auto", marginRight: "auto", marginTop: "15%", color: "$accent1"}}>
-                    Current threshold: {threshold}
-                </Heading>
-
-                <Button
-                    color="accent"
-                    onPress={getThreshold}
-                    size="lg"
-                    variant="solid"
-                >
-                    Get threshold
                 </Button>
 
             </Stack>
