d8dbe798198a038e3a0e5bc273079eb6308259ea
Kyran Rawlinson <kyranrawlinson@gmail.com>
2023-05-14 22:32:34 +0100

    Update malicious contract paths

--- AMM/project/test-utils/src/paths.rs
+++ AMM/project/test-utils/src/paths.rs
@@ -8,8 +8,8 @@ pub const EXCHANGE_CONTRACT_BINARY_PATH: &str =
 pub const EXCHANGE_CONTRACT_STORAGE_PATH: &str =
     "../../contracts/exchange-contract/out/debug/exchange-contract-storage_slots.json";
 pub const MALICIOUS_EXCHANGE_CONTRACT_BINARY_PATH: &str =
-    "../exchange-contract/tests/artifacts/malicious-implementation/out/debug/malicious-implementation.bin";
+    "../../contracts/test-artifacts/malicious-implementation/out/debug/malicious-implementation.bin";
 pub const MALICIOUS_EXCHANGE_CONTRACT_STORAGE_PATH: &str =
-    "../exchange-contract/tests/artifacts/malicious-implementation/out/debug/malicious-implementation-storage_slots.json";
+    "../../contracts/test-artifacts/malicious-implementation/out/debug/malicious-implementation-storage_slots.json";
 pub const SWAP_EXACT_INPUT_SCRIPT_BINARY_PATH: &str = "./out/debug/swap-exact-input.bin";
 pub const SWAP_EXACT_OUTPUT_SCRIPT_BINARY_PATH: &str = "./out/debug/swap-exact-output.bin";
