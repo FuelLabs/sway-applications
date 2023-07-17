7617995fb3db2855869a94fc90a1c2765e5c0b47
Kyran Rawlinson <kyranrawlinson@gmail.com>
2023-07-17 14:38:31 +0100

    Address test comments

--- AMM/project/scripts/swap-exact-input/tests/utils/mod.rs
+++ AMM/project/scripts/swap-exact-input/tests/utils/mod.rs
@@ -38,8 +38,7 @@ pub async fn expected_swap_output(
 }
 
 pub async fn expected_and_actual_output(swap_parameters: SwapParameters) -> SwapResult {
-    let (script_instance, script_configurables, amm, asset_ids, transaction_parameters, deadline) =
-        setup().await;
+    let (script_instance, amm, asset_ids, transaction_parameters, deadline) = setup().await;
 
     let mut route = Vec::with_capacity(swap_parameters.route_length as usize);
     let mut asset_index = 0;
@@ -55,7 +54,6 @@ pub async fn expected_and_actual_output(swap_parameters: SwapParameters) -> Swap
     };
 
     let actual = script_instance
-        .with_configurables(script_configurables)
         .main(
             route
                 .into_iter()
@@ -79,7 +77,6 @@ pub async fn expected_and_actual_output(swap_parameters: SwapParameters) -> Swap
 
 pub async fn setup() -> (
     SwapExactInputScript<WalletUnlocked>,
-    SwapExactInputScriptConfigurables,
     AMMContract,
     Vec<AssetId>,
     TransactionParameters,
@@ -100,14 +97,14 @@ pub async fn setup() -> (
 
     let deadline = provider.latest_block_height().await.unwrap() + 10;
 
-    let script_instance = SwapExactInputScript::new(wallet, SWAP_EXACT_INPUT_SCRIPT_BINARY_PATH);
-
     let script_configurables = SwapExactInputScriptConfigurables::new()
         .set_AMM_ID(Bits256::from_hex_str(&amm.id.to_string()).unwrap());
 
+    let script_instance = SwapExactInputScript::new(wallet, SWAP_EXACT_INPUT_SCRIPT_BINARY_PATH)
+        .with_configurables(script_configurables);
+
     (
         script_instance,
-        script_configurables,
         amm,
         asset_ids,
         transaction_parameters,
