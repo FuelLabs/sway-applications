bfbee8a5ddf261a1d9ea3d962b627d3e5aed9780
Kyran Rawlinson <kyranrawlinson@gmail.com>
2023-07-19 15:49:12 +0100

    Added panic message back in to test and refactor setup

--- DAO/project/contracts/DAO-contract/tests/utils/setup.rs
+++ DAO/project/contracts/DAO-contract/tests/utils/setup.rs
@@ -1,7 +1,9 @@
 use fuels::prelude::{
-    abigen, setup_multiple_assets_coins, setup_test_provider, AssetId, Contract, ContractId,
-    LoadConfiguration, StorageConfiguration, TxParameters, WalletUnlocked,
+    abigen, launch_custom_provider_and_get_wallets, AssetConfig, AssetId, Contract, ContractId,
+    LoadConfiguration, StorageConfiguration, TxParameters, WalletUnlocked, WalletsConfig,
+    BASE_ASSET_ID,
 };
+use rand::Fill;
 
 abigen!(Contract(
     name = "DaoVoting",
@@ -32,16 +34,41 @@ pub(crate) fn proposal_transaction(asset_id: ContractId) -> Proposal {
 }
 
 pub(crate) async fn setup() -> (ContractId, ContractId, Metadata, Metadata, u64) {
-    let (mut wallets, mut asset_ids) = setup_wallets().await;
+    let mut rng = rand::thread_rng();
+    let num_coins = 1;
+    let coin_amount = 1_000_000;
+
+    let base_asset = AssetConfig {
+        id: BASE_ASSET_ID,
+        num_coins,
+        coin_amount,
+    };
+    let mut gov_token_id = AssetId::zeroed();
+    gov_token_id.try_fill(&mut rng).unwrap();
+    let gov_token = AssetConfig {
+        id: gov_token_id,
+        num_coins,
+        coin_amount,
+    };
+    let mut other_token_id = AssetId::zeroed();
+    other_token_id.try_fill(&mut rng).unwrap();
+    let other_token = AssetConfig {
+        id: other_token_id,
+        num_coins,
+        coin_amount,
+    };
+    let assets = vec![base_asset, gov_token, other_token];
+
+    let num_wallets = 2;
+    let wallet_config = WalletsConfig::new_multiple_assets(num_wallets, assets);
+    let mut wallets = launch_custom_provider_and_get_wallets(wallet_config, None, None).await;
 
     let deployer_wallet = wallets.pop().unwrap();
     let user_wallet = wallets.pop().unwrap();
 
     let storage_configuration = StorageConfiguration::load_from(DAO_CONTRACT_STORAGE_PATH);
-
     let configuration =
         LoadConfiguration::default().set_storage_configuration(storage_configuration.unwrap());
-
     let dao_voting_id = Contract::load_from(DAO_CONTRACT_BINARY_PATH, configuration)
         .unwrap()
         .deploy(&deployer_wallet, TxParameters::default())
@@ -52,7 +79,6 @@ pub(crate) async fn setup() -> (ContractId, ContractId, Metadata, Metadata, u64)
         dao_voting: DaoVoting::new(dao_voting_id.clone(), deployer_wallet.clone()),
         wallet: deployer_wallet,
     };
-
     let user = Metadata {
         dao_voting: DaoVoting::new(dao_voting_id, user_wallet.clone()),
         wallet: user_wallet,
@@ -60,48 +86,11 @@ pub(crate) async fn setup() -> (ContractId, ContractId, Metadata, Metadata, u64)
 
     let asset_amount = 10;
 
-    let gov_token_id: [u8; 32] = asset_ids.pop().unwrap().into();
-    let other_token_id: [u8; 32] = asset_ids.pop().unwrap().into();
-
     (
-        gov_token_id.into(),
-        other_token_id.into(),
+        ContractId::from(*gov_token_id),
+        ContractId::from(*other_token_id),
         deployer,
         user,
         asset_amount,
     )
-}
-
-pub(crate) async fn setup_wallets() -> (Vec<WalletUnlocked>, Vec<AssetId>) {
-    let mut wallets = vec![
-        WalletUnlocked::new_random(None),
-        WalletUnlocked::new_random(None),
-    ];
-
-    let num_assets = 3;
-    let coins_per_asset = 1;
-    let amount_per_coin = 1_000_000;
-
-    let mut all_coins = vec![];
-    let mut asset_ids = vec![];
-
-    for wallet in &mut wallets {
-        let (mut coin, assets) = setup_multiple_assets_coins(
-            wallet.address(),
-            num_assets,
-            coins_per_asset,
-            amount_per_coin,
-        );
-
-        all_coins.append(&mut coin);
-        asset_ids = assets;
-    }
-
-    let (provider, _) = setup_test_provider(all_coins, vec![], None, None).await;
-
-    for wallet in &mut wallets {
-        wallet.set_provider(provider.clone());
-    }
-
-    (wallets, asset_ids)
 }
