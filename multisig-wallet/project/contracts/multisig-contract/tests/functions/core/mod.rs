8d4f0bcabce25b07802437691d3f60328d674b42
K1-R1 <k1r1.devwork@gmail.com>
2023-07-31 22:32:54 +0100

    Removed deprectated function support and refactored

--- multisig-wallet/project/contracts/multisig-contract/tests/functions/core/mod.rs
+++ multisig-wallet/project/contracts/multisig-contract/tests/functions/core/mod.rs
@@ -1,5 +1,3 @@
 mod constructor;
-mod execute_transaction;
-mod set_threshold;
-mod set_weight;
-mod transfer;
+// mod set_threshold;
+// mod set_weight;
--- multisig-wallet/project/contracts/multisig-contract/tests/utils/interface/core.rs
+++ multisig-wallet/project/contracts/multisig-contract/tests/utils/interface/core.rs
@@ -14,31 +14,15 @@ pub(crate) async fn constructor(
     contract.methods().constructor(users).call().await.unwrap()
 }
 
-pub(crate) async fn _execute_transaction(
-    contract: &MultiSig<WalletUnlocked>,
-    to: Identity,
-    value: u64,
-    data: Bits256,
-    signatures_data: Vec<SignatureInfo>,
-) -> FuelCallResponse<()> {
-    contract
-        .methods()
-        .execute_transaction(data, signatures_data, to, value)
-        .append_variable_outputs(1)
-        .call()
-        .await
-        .unwrap()
-}
-
 pub(crate) async fn set_threshold(
     contract: &MultiSig<WalletUnlocked>,
-    data: Option<Bits256>,
+
     signatures_data: Vec<SignatureInfo>,
     threshold: u64,
 ) -> FuelCallResponse<()> {
     contract
         .methods()
-        .set_threshold(data, signatures_data, threshold)
+        .set_threshold(signatures_data, threshold)
         .call()
         .await
         .unwrap()
@@ -46,30 +30,13 @@ pub(crate) async fn set_threshold(
 
 pub(crate) async fn set_weight(
     contract: &MultiSig<WalletUnlocked>,
-    data: Option<Bits256>,
+
     signatures_data: Vec<SignatureInfo>,
     user: User,
 ) -> FuelCallResponse<()> {
     contract
         .methods()
-        .set_weight(data, signatures_data, user)
-        .call()
-        .await
-        .unwrap()
-}
-
-pub(crate) async fn transfer(
-    contract: &MultiSig<WalletUnlocked>,
-    to: Identity,
-    asset_id: ContractId,
-    value: u64,
-    data: Bits256,
-    signatures_data: Vec<SignatureInfo>,
-) -> FuelCallResponse<()> {
-    contract
-        .methods()
-        .transfer(asset_id, data, signatures_data, to, value)
-        .append_variable_outputs(1)
+        .set_weight(signatures_data, user)
         .call()
         .await
         .unwrap()
