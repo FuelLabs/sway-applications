cd570e0d3b4f7281e613fd78e206aaae8d9340ab
bitzoic <bitzoic.eth@gmail.com>
2024-01-10 10:34:10 +0700

    Update test utils and interfaces

--- native-assets/fractional-NFT/project/contracts/f-NFT-contract/tests/utils/interface.rs
+++ native-assets/fractional-NFT/project/contracts/f-NFT-contract/tests/utils/interface.rs
@@ -1,24 +1,119 @@
 use crate::utils::setup::F_NFT;
 use fuels::{
-    prelude::{AssetId, CallParameters, TxPolicies, WalletUnlocked, BASE_ASSET_ID},
-    programs::{call_response::FuelCallResponse, call_utils::TxDependencyExtension},
+    prelude::{AssetId, CallParameters, TxPolicies, WalletUnlocked},
+    programs::call_utils::TxDependencyExtension,
     types::{Bits256, Identity},
+    tx::Bytes32,
 };
 
-pub(crate) async fn start_buyback(
+pub(crate) async fn deposit(
     contract: &F_NFT<WalletUnlocked>,
-    nft: AssetId, 
-    token_buyback_price: u64,
-    f_nft_supply: u64,
-) -> FuelCallResponse<()> {
-    let call_params = CallParameters::new((f_nft_supply * token_buyback_price) - , BASE_ASSET_ID, 1_000_000);
+    nft: AssetId,
+    reciever: Identity,
+    vault_sub_id: Bytes32,
+) -> u64 {
+    let call_params = CallParameters::new(1 , nft, 1_000_000);
 
     contract
         .methods()
-        .start_buyback(nft, token_buyback_price)
-        //.with_tx_policies(TxPolicies::default().with_script_gas_limit(2_000_000))
+        .deposit(reciever, Bits256(*vault_sub_id))
+        .with_tx_policies(TxPolicies::default().with_script_gas_limit(2_000_000))
         .call_params(call_params)
+        .unwrap()
+        .append_variable_outputs(1)
         .call()
         .await
         .unwrap()
+        .value
+}
+
+pub(crate) async fn withdraw(
+    contract: &F_NFT<WalletUnlocked>,
+    share_asset: AssetId,
+    amount: u64,
+    reciever: Identity,
+    underlying_asset: AssetId,
+    vault_sub_id: Bytes32,
+) -> u64 {
+    let call_params = CallParameters::new(amount, share_asset, 1_000_000);
+
+    contract
+        .methods()
+        .withdraw(reciever, underlying_asset, Bits256(*vault_sub_id))
+        .with_tx_policies(TxPolicies::default().with_script_gas_limit(2_000_000))
+        .call_params(call_params)
+        .unwrap()
+        .append_variable_outputs(1)
+        .call()
+        .await
+        .unwrap()
+        .value
+}
+
+pub(crate) async fn managed_assets(
+    contract: &F_NFT<WalletUnlocked>,
+    underlying_asset: AssetId,
+    vault_sub_id: Bytes32,
+) -> u64 {
+    contract
+        .methods()
+        .managed_assets(underlying_asset, Bits256(*vault_sub_id))
+        .call()
+        .await
+        .unwrap()
+        .value
+}
+
+pub(crate) async fn max_depositable(
+    contract: &F_NFT<WalletUnlocked>,
+    reciever: Identity,
+    underlying_asset: AssetId,
+    vault_sub_id: Bytes32,
+) -> Option<u64> {
+    contract
+        .methods()
+        .max_depositable(reciever, underlying_asset, Bits256(*vault_sub_id))
+        .call()
+        .await
+        .unwrap()
+        .value
+}
+
+pub(crate) async fn max_withdrawable(
+    contract: &F_NFT<WalletUnlocked>,
+    underlying_asset: AssetId,
+    vault_sub_id: Bytes32,
+) -> Option<u64> {
+    contract
+        .methods()
+        .max_withdrawable(underlying_asset, Bits256(*vault_sub_id))
+        .call()
+        .await
+        .unwrap()
+        .value
+}
+
+pub(crate) async fn total_assets(
+    contract: &F_NFT<WalletUnlocked>,
+) -> u64 {
+    contract
+        .methods()
+        .total_assets()
+        .call()
+        .await
+        .unwrap()
+        .value
+}
+
+pub(crate) async fn total_supply(
+    contract: &F_NFT<WalletUnlocked>,
+    share_asset: AssetId,
+) -> Option<u64> {
+    contract
+        .methods()
+        .total_supply(share_asset)
+        .call()
+        .await
+        .unwrap()
+        .value
 }
--- native-assets/fractional-NFT/project/contracts/f-NFT-contract/tests/utils/setup.rs
+++ native-assets/fractional-NFT/project/contracts/f-NFT-contract/tests/utils/setup.rs
@@ -1,6 +1,7 @@
 use fuels::{
+    accounts::ViewOnlyAccount,
     prelude::{
-        abigen, launch_custom_provider_and_get_wallets, AssetConfig, Bech32ContractId, Contract,
+        abigen, launch_custom_provider_and_get_wallets, AssetConfig, Contract,
         ContractId, LoadConfiguration, StorageConfiguration, TxPolicies, WalletUnlocked,
         WalletsConfig, BASE_ASSET_ID,
     },
@@ -21,9 +22,9 @@ Contract(
 );
 
 const F_NFT_CONTRACT_BINARY_PATH: &str = "./out/debug/f-NFT-contract.bin";
-const NFT_CONTRACT_BINARY_PATH: &str = "../../NFT/project/contracts/NFT-contract/out/debug/NFT-contract.bin";
+const NFT_CONTRACT_BINARY_PATH: &str = "../../../../NFT/project/contracts/NFT-contract/out/debug/NFT-contract.bin";
 const F_NFT_CONTRACT_STORAGE_PATH: &str = "./out/debug/f-NFT-contract-storage_slots.json";
-const NFT_CONTRACT_STORAGE_PATH: &str = "../../NFT/project/contracts/NFT-contract/out/debug/NFT-contract-storage_slots.json";
+const NFT_CONTRACT_STORAGE_PATH: &str = "../../../../NFT/project/contracts/NFT-contract/out/debug/NFT-contract-storage_slots.json";
 
 pub(crate) struct User {
     pub(crate) f_nft: F_NFT<WalletUnlocked>,
@@ -31,43 +32,36 @@ pub(crate) struct User {
     pub(crate) wallet: WalletUnlocked,
 }
 
-pub(crate) async fn setup_nft(wallet: WalletUnlocked, nft: &NFT<WalletUnlocked>, nft_id: ContractId) -> (AssetId, AssetId, AssetId) {
-    let identity = Identity::Address(wallet.address().into());
-    let b256_1 = Bytes32::new([1u8; 32]);
-    let b256_2 = Bytes32::new([2u8; 32]);
+pub(crate) fn defaults(wallet: &WalletUnlocked, nft_1: AssetId, nft_2: AssetId, f_nft_id: ContractId) ->
+    (Bytes32,
+    Identity,
+    AssetId,
+    AssetId,
+    u64)
+{
+    let vault_sub_id = Bytes32::zeroed();
+    let vault_admin = Identity::Address(wallet.address().into());
+
+    let share_asset1_sub_id = get_share_sub_id(vault_sub_id, nft_1);
+    let share_asset1 = get_asset_id(share_asset1_sub_id, f_nft_id);
+
+    let share_asset2_sub_id = get_share_sub_id(vault_sub_id, nft_2);
+    let share_asset2 = get_asset_id(share_asset2_sub_id, f_nft_id);
     
-    let _ = nft.methods().mint(identity.clone(), Bits256(*Bytes32::zeroed()), 1).append_variable_outputs(1).call().await;
-    let _ = nft.methods().mint(identity.clone(), Bits256(*b256_1), 1).append_variable_outputs(1).call().await;
-    let _ = nft.methods().mint(identity, Bits256(*b256_2), 1).append_variable_outputs(1).call().await;
+    let share_supply = 100_000_000;
 
-    let mut hasher1 = Sha256::new();
-    hasher1.update(*nft_id);
-    hasher1.update(*Bytes32::zeroed());
-    let nft_1 = AssetId::new(hasher1.finalize().try_into().unwrap());
-
-    let mut hasher2 = Sha256::new();
-    hasher2.update(*nft_id);
-    hasher2.update(*b256_1);
-    let nft_2 = AssetId::new(hasher2.finalize().try_into().unwrap());
-
-    let mut hasher3 = Sha256::new();
-    hasher3.update(*nft_id);
-    hasher3.update(*b256_2);
-    let nft_3 = AssetId::new(hasher3.finalize().try_into().unwrap());
-
-    (nft_1, nft_2, nft_3)
+    (vault_sub_id, vault_admin, share_asset1, share_asset2, share_supply)
 }
 
 pub(crate) async fn deploy() -> (
-    User,
     User,
     User,
     ContractId,
     ContractId,
 ) {
     let number_of_coins = 1;
-    let coin_amount = 1_000_000;
-    let number_of_wallets = 3;
+    let coin_amount = 100_000_000;
+    let number_of_wallets = 2;
 
     let base_asset = AssetConfig {
         id: BASE_ASSET_ID,
@@ -83,7 +77,6 @@ pub(crate) async fn deploy() -> (
 
     let wallet1 = wallets.pop().unwrap();
     let wallet2 = wallets.pop().unwrap();
-    let wallet3 = wallets.pop().unwrap();
 
     let nft_storage_configuration =
         StorageConfiguration::default().add_slot_overrides_from_file(NFT_CONTRACT_STORAGE_PATH);
@@ -131,23 +124,42 @@ pub(crate) async fn deploy() -> (
         wallet: wallet2,
     };
 
-    let user3 = User {
-        f_nft: F_NFT::new(
-            f_nft_id.clone(),
-            wallet3.clone(),
-        ),
-        nft: NFT::new(
-            nft_id.clone(),
-            wallet3.clone(),
-        ),
-        wallet: wallet3,
-    };
-
     (
         user1,
         user2,
-        user3,
         f_nft_id.into(),
         nft_id.into(),
     )
+}
+
+pub(crate) fn get_asset_id(sub_id: Bytes32, contract: ContractId) -> AssetId {
+    let mut hasher = Sha256::new();
+    hasher.update(*contract);
+    hasher.update(*sub_id);
+    AssetId::new(*Bytes32::from(<[u8; 32]>::from(hasher.finalize())))
+}
+
+pub(crate) fn get_share_sub_id(vault_sub_id: Bytes32, nft: AssetId) -> Bytes32 {
+    let mut hasher = Sha256::new();
+    hasher.update(*nft);
+    hasher.update(*vault_sub_id);
+    Bytes32::from(<[u8; 32]>::from(hasher.finalize()))
+}
+
+pub(crate) async fn get_wallet_balance(wallet: &WalletUnlocked, asset: &AssetId) -> u64 {
+    wallet.get_asset_balance(asset).await.unwrap()
+}
+
+pub(crate) async fn setup_nft(wallet: &WalletUnlocked, nft: &NFT<WalletUnlocked>, nft_id: ContractId) -> (AssetId, AssetId) {
+    let identity = Identity::Address(wallet.address().into());
+    let sub_id_1 = Bytes32::zeroed();
+    let sub_id_2 = Bytes32::new([1u8; 32]);
+    
+    let _ = nft.methods().mint(identity.clone(), Bits256(*sub_id_1), 1).append_variable_outputs(1).call().await;
+    let _ = nft.methods().mint(identity.clone(), Bits256(*sub_id_2), 1).append_variable_outputs(1).call().await;
+
+    let nft_1 = get_asset_id(sub_id_1, nft_id);
+    let nft_2 = get_asset_id(sub_id_2, nft_id);
+
+    (nft_1, nft_2)
 }
