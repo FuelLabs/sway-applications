3ed80f3358bb6659a5598d2501c78ac168157dbb
Matt Auer <mattauer@umich.edu>
2024-03-11 16:03:30 -0500

    fix: ci

--- games/TicTacToe/packages/app/fuels.config.ts
+++ games/TicTacToe/packages/app/fuels.config.ts
@@ -1,7 +1,7 @@
 import { createConfig } from 'fuels';
 
 export default createConfig({
-  workspace: '../tictactoe-contracts',
+  workspace: '../project',
   output: './src/contract-types',
   useBuiltinForc: false,
   useBuiltinFuelCore: true,
