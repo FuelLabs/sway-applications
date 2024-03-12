12b29b36aae4f021745f9e6e17e4430f0689c79f
Matt Auer <mattauer@umich.edu>
2024-03-12 14:57:09 -0500

    Refactor: for vercel integration

--- games/TicTacToe/app/src/main.tsx
+++ games/TicTacToe/app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { Providers } from "./components";
+import App from "./App.js";
+import { Providers } from "./components/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
