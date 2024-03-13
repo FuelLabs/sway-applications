68a2796e1cdeb1bbf4ff8b9c9fc41b395a94adc5
Matt Auer <mattauer@umich.edu>
2024-03-13 16:19:53 -0500

    ci: add deploy contract workflow

--- games/TicTacToe/frontend-app/src/config.ts
+++ games/TicTacToe/frontend-app/src/config.ts
@@ -1,5 +1,7 @@
 import contractIds from "./contract-types/contract-ids.json";
+import productionContractIds from "../production-contract/contract-ids.json";
 
-export const CONTRACT_ID = contractIds.tictactoeContract;
 export const NODE_ENV = import.meta.env.NODE_ENV;
-export const PROVIDER_URL = NODE_ENV === "production" ? "https://beta-5.fuel.network/graphql" : "http://localhost:4000/graphql";
+const isProd = NODE_ENV === "production";
+export const CONTRACT_ID = isProd ? productionContractIds.tictactoeContract : contractIds.tictactoeContract;
+export const PROVIDER_URL = isProd ? "https://beta-5.fuel.network/graphql" : "http://localhost:4000/graphql";
