0dc283ee19fc4d00da3983924a73e9fcb891c918
bitzoic <bitzoic.eth@gmail.com>
2024-03-22 11:50:29 +0900

    Reorg AMM

--- AMM/test-utils/src/paths.rs
+++ AMM/test-utils/src/paths.rs
@@ -1,15 +1,15 @@
 pub const AMM_CONTRACT_BINARY_PATH: &str =
-    "../../contracts/AMM-contract/out/debug/AMM-contract.bin";
+    "../AMM-contract/out/debug/AMM-contract.bin";
 pub const AMM_CONTRACT_STORAGE_PATH: &str =
-    "../../contracts/AMM-contract/out/debug/AMM-contract-storage_slots.json";
+    "../AMM-contract/out/debug/AMM-contract-storage_slots.json";
 pub const ATOMIC_ADD_LIQUIDITY_SCRIPT_BINARY_PATH: &str = "./out/debug/atomic-add-liquidity.bin";
 pub const EXCHANGE_CONTRACT_BINARY_PATH: &str =
-    "../../contracts/exchange-contract/out/debug/exchange-contract.bin";
+    "../exchange-contract/out/debug/exchange-contract.bin";
 pub const EXCHANGE_CONTRACT_STORAGE_PATH: &str =
-    "../../contracts/exchange-contract/out/debug/exchange-contract-storage_slots.json";
+    "../exchange-contract/out/debug/exchange-contract-storage_slots.json";
 pub const MALICIOUS_EXCHANGE_CONTRACT_BINARY_PATH: &str =
-    "../../contracts/test-artifacts/malicious-implementation/out/debug/malicious-implementation.bin";
+    "../test-utils/test-artifacts/malicious-implementation/out/debug/malicious-implementation.bin";
 pub const MALICIOUS_EXCHANGE_CONTRACT_STORAGE_PATH: &str =
-    "../../contracts/test-artifacts/malicious-implementation/out/debug/malicious-implementation-storage_slots.json";
+    "../test-utils/test-artifacts/malicious-implementation/out/debug/malicious-implementation-storage_slots.json";
 pub const SWAP_EXACT_INPUT_SCRIPT_BINARY_PATH: &str = "./out/debug/swap-exact-input.bin";
 pub const SWAP_EXACT_OUTPUT_SCRIPT_BINARY_PATH: &str = "./out/debug/swap-exact-output.bin";
