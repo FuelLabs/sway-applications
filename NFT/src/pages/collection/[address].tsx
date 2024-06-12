d1e4c4f765215527da19865fa5e4f656fe5f37bd
Matt Auer <mattauer@umich.edu>
2024-06-12 13:50:14 -0500

    feat: add user nft page

--- NFT/src/pages/collection/[address].tsx
+++ NFT/src/pages/collection/[address].tsx
@@ -1,6 +1,6 @@
 import { NFTCard } from "@/components/NFTCard";
 import { useGetNFTData } from "@/hooks/useGetNFTData";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 
 export default function Collection() {
@@ -19,22 +19,31 @@ export default function Collection() {
   console.log(`nftData`, nftData);
 
   return (
-    <Grid container spacing={2}>
-      {nftData ? nftData.map((nftDatum) => {
-        return (
-          <Grid xs={3}>
-            <NFTCard
-              cid={nftDatum.ipfs_pin_hash}
-              fileCid={nftDatum.metadata?.name || ""}
-              nftName={nftDatum.metadata.keyvalues?.nftName || ""}
-              nftDescription={nftDatum.metadata.keyvalues?.nftDescription || ""}
-              nftSubId={nftDatum.metadata.keyvalues?.nftSubId || ""}
-            />
+    <>
+      {nftData ? (
+        <Stack>
+          <Typography>Your NFTs</Typography>
+          <Grid container spacing={2}>
+            {nftData.map((nftDatum) => {
+              return (
+                <Grid xs={3}>
+                  <NFTCard
+                    cid={nftDatum.ipfs_pin_hash}
+                    fileCid={nftDatum.metadata?.name || ""}
+                    nftName={nftDatum.metadata.keyvalues?.nftName || ""}
+                    nftDescription={
+                      nftDatum.metadata.keyvalues?.nftDescription || ""
+                    }
+                    nftSubId={nftDatum.metadata.keyvalues?.nftSubId || ""}
+                  />
+                </Grid>
+              );
+            })}
           </Grid>
-        );
-      }) : (
+        </Stack>
+      ) : (
         <Typography>No NFTs found</Typography>
       )}
-    </Grid>
+    </>
   );
 }
