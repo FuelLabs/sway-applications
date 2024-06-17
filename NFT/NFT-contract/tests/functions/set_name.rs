100a2dc50fe720160e122aa7a257266f7f95164f
Matt Auer <mattauer@umich.edu>
2024-06-17 12:46:03 -0500

    test: remove old tests

--- NFT/NFT-contract/tests/functions/set_name.rs
+++ NFT/NFT-contract/tests/functions/set_name.rs
@@ -128,26 +128,6 @@ mod revert {
     use super::*;
 
     #[tokio::test]
-    #[should_panic(expected = "NotOwner")]
-    async fn when_not_owner() {
-        let (owner_wallet, other_wallet, id, instance_1, instance_2) = setup().await;
-        let (
-            asset_id_1,
-            _asset_id_2,
-            _asset_id_3,
-            _sub_id_1,
-            _sub_id_2,
-            _sub_id_3,
-            owner_identity,
-            _other_identity,
-        ) = defaults(id, owner_wallet, other_wallet.clone());
-
-        constructor(&instance_1, owner_identity).await;
-
-        set_name(&instance_2, asset_id_1, String::from("Fuel NFT 1")).await;
-    }
-
-    #[tokio::test]
     #[should_panic(expected = "ValueAlreadySet")]
     async fn when_a_name_has_already_been_set() {
         let (owner_wallet, other_wallet, id, instance_1, _instance_2) = setup().await;
